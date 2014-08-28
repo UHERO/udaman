8a3cf0df66cef1358552e9353abc13b4f3af71d8
Jonathan Page <jonpage.econ@gmail.com>
2014-08-28 11:25:03 -1000

    removed @mailer_error from mailer models and views

--- app/mailers/data_load_mailer.rb
+++ app/mailers/data_load_mailer.rb
@@ -1,29 +1,28 @@
 class DataLoadMailer < ActionMailer::Base
   default :from => "udaman@hawaii.edu"
-  
+
   def download_notification(update_info, update_summary)
     begin
-        @update_info = update_info
-        @update_summary = update_summary
-        mail(:to => ["jrpage@hawaii.edu", "bentut@gmail.com"], :subject => "A spreadsheet was updated (Udamacmini): #{update_info}")
+      @update_info = update_info
+      @update_summary = update_summary
+      mail(:to => ["jrpage@hawaii.edu", "bentut@gmail.com"], :subject => "A spreadsheet was updated (Udamacmini): #{update_info}")
     rescue => e
-        @mailer_error = e.message
-        mail(:to => ["jrpage@hawaii.edu", "bentut@gmail.com"], :subject => "[UDAMACMINI] DataLoadMailer error")
+      mail(:to => ["jrpage@hawaii.edu", "bentut@gmail.com"], :subject => "[UDAMACMINI] DataLoadMailer error", :body => e.message, :content_type => "text/plain")
     end
   end
-  
+
   def series_refresh_notification(circular_series, stale_data_sources, ds_count, errors)
     begin
-        @circular_series = circular_series
-        @stale_data_sources = stale_data_sources
-        #add this in later
-        #@empty_data_sources = empty_data_sources
+      @circular_series = circular_series
+      @stale_data_sources = stale_data_sources
+      #add this in later
+      #@empty_data_sources = empty_data_sources
 
-        @errors = errors
-        mail(:to => ["jrpage@hawaii.edu", "bentut@gmail.com"], :subject => "[Udamacmini Series Reload] #{ds_count} Datasources loaded : #{@errors.count} / #{@circular_series.count} / #{@stale_data_sources.count} ")
+      @errors = errors
+      mail(:to => ["jrpage@hawaii.edu", "bentut@gmail.com"], :subject => "[Udamacmini Series Reload] #{ds_count} Datasources loaded : #{@errors.count} / #{@circular_series.count} / #{@stale_data_sources.count} ")
     rescue => e
-        @mailer_error = e.message
-        mail(:to => ["jrpage@hawaii.edu", "bentut@gmail.com"], :subject => "[UDAMACMINI] DataLoadMailer error")
+      @mailer_error = e.message
+      mail(:to => ["jrpage@hawaii.edu", "bentut@gmail.com"], :subject => "[UDAMACMINI] DataLoadMailer error", :body => e.message, :content_type => "text/plain")
     end
   end
 end
--- app/mailers/packager_mailer.rb
+++ app/mailers/packager_mailer.rb
@@ -1,6 +1,6 @@
 class PackagerMailer < ActionMailer::Base
   default :from => "udaman@hawaii.edu"
-  
+
   # def rake_notification(rake_task, download_string, error_string, summary_string)
   #   @download_string = download_string
   #   @error_string = error_string
@@ -10,80 +10,74 @@ class PackagerMailer < ActionMailer::Base
 
   def rake_notification(rake_task, download_results, errors, series, output_path, is_error)
     begin
-        @download_results = download_results
-        @errors = errors
-        @series = series
-        @output_path = output_path
-        @dates = Series.get_all_dates_from_data(@series)
-        subject = "UDAMacMini Error (#{rake_task})" if is_error
-        subject = "UDAMacMini New Download (#{rake_task})" unless is_error
-        mail(:to => ["btrevino@hawaii.edu", "jrpage@hawaii.edu"], :subject => subject)
+      @download_results = download_results
+      @errors = errors
+      @series = series
+      @output_path = output_path
+      @dates = Series.get_all_dates_from_data(@series)
+      subject = "UDAMacMini Error (#{rake_task})" if is_error
+      subject = "UDAMacMini New Download (#{rake_task})" unless is_error
+      mail(:to => ["btrevino@hawaii.edu", "jrpage@hawaii.edu"], :subject => subject)
     rescue => e
-        @mailer_error = e.message
-        mail(:to => ["btrevino@hawaii.edu", "jrpage@hawaii.edu"], :subject => "[UDAMACMINI] PackageMailer.rake_notification error")
+      mail(:to => ["btrevino@hawaii.edu", "jrpage@hawaii.edu"], :subject => "[UDAMACMINI] PackageMailer.rake_notification error", :body => e.message, :content_type => "text/plain")
     end
   end
-  
+
   def rake_error(e, output_path)
-      begin
-    @error = e
-    @output_path = output_path
-    mail(:to => ["btrevino@hawaii.edu", "jrpage@hawaii.edu"], :subject => "Rake failed in an unexpected way (Udamacmini)")
-      rescue => e
-        @mailer_error = e.message
-          mail(:to => ["btrevino@hawaii.edu", "jrpage@hawaii.edu"], :subject => "[UDAMACMINI] PackageMailer.rake_error error")
-      end
+    begin
+      @error = e
+      @output_path = output_path
+      mail(:to => ["btrevino@hawaii.edu", "jrpage@hawaii.edu"], :subject => "Rake failed in an unexpected way (Udamacmini)")
+    rescue => e
+      mail(:to => ["btrevino@hawaii.edu", "jrpage@hawaii.edu"], :subject => "[UDAMACMINI] PackageMailer.rake_error error", :body => e.message, :content_type => "text/plain")
+    end
   end
 
   def visual_notification(new_dps = 0, changed_files = 0, new_downloads = 0)
-      begin
-          attachments.inline['photo.png'] = File.read(Rails.root.to_s + '/script/investigate_visual.png')
-          attachments['photo.png'] = File.read(Rails.root.to_s + '/script/investigate_visual.png')
-          subject = "Udamacmini Download Report: #{new_dps.to_s + " new data points / " unless new_dps == 0} #{new_downloads.to_s + " updated downloads / " unless new_downloads == 0} #{changed_files.to_s + " modified update spreadsheets" unless changed_files == 0}"
-          mail(:to => ["btrevino@hawaii.edu", "jrpage@hawaii.edu", "james29@hawaii.edu", "fuleky@hawaii.edu", "ashleysh@hawaii.edu"], :subject => subject)
-      rescue => e
-        @mailer_error = e.message
-          mail(:to => ["btrevino@hawaii.edu", "jrpage@hawaii.edu"], :subject => "[UDAMACMINI] PackageMailer.visual_notification")
-      end
+    begin
+      attachments.inline['photo.png'] = File.read(Rails.root.to_s + '/script/investigate_visual.png')
+      attachments['photo.png'] = File.read(Rails.root.to_s + '/script/investigate_visual.png')
+      subject = "Udamacmini Download Report: #{new_dps.to_s + " new data points / " unless new_dps == 0} #{new_downloads.to_s + " updated downloads / " unless new_downloads == 0} #{changed_files.to_s + " modified update spreadsheets" unless changed_files == 0}"
+      mail(:to => ["btrevino@hawaii.edu", "jrpage@hawaii.edu", "james29@hawaii.edu", "fuleky@hawaii.edu", "ashleysh@hawaii.edu"], :subject => subject)
+    rescue => e
+      mail(:to => ["btrevino@hawaii.edu", "jrpage@hawaii.edu"], :subject => "[UDAMACMINI] PackageMailer.visual_notification error", :body => e.message, :content_type => "text/plain")
+    end
   end
 
   def download_link_notification(handle = nil, url = nil, save_path = nil, created = false)
-      begin
-          subject = "Udamacmini found and created a new download link for #{handle}" if created
-          subject = "Udamacmini tried but failed to create a new download link for #{handle}" unless created
-          @handle = handle
-          @url = url
-          @save_path = save_path
-          mail(:to => ["jrpage@hawaii.edu", "btrevino@hawaii.edu"], :subject => subject)
-      rescue => e
-        @mailer_error = e.message
-          mail(:to => ["btrevino@hawaii.edu", "jrpage@hawaii.edu"], :subject => "[UDAMACMINI] PackageMailer.download_link_notification")
-      end
+    begin
+      subject = "Udamacmini found and created a new download link for #{handle}" if created
+      subject = "Udamacmini tried but failed to create a new download link for #{handle}" unless created
+      @handle = handle
+      @url = url
+      @save_path = save_path
+      mail(:to => ["jrpage@hawaii.edu", "btrevino@hawaii.edu"], :subject => subject)
+    rescue => e
+      mail(:to => ["btrevino@hawaii.edu", "jrpage@hawaii.edu"], :subject => "[UDAMACMINI] PackageMailer.download_link_notification error", :body => e.message, :content_type => "text/plain")
+    end
   end
-  
+
   def website_post_notification(post_name, post_address, new_data_series, created)
-      begin
-    subject = "Udamacmini: New data for #{post_name} posted to the UHERO website" if created
-    subject = "Udamacmini tried but failed to post new data for #{post_name} to the UHERO website" unless created
-    @post_address = post_address
-    @new_data_series = new_data_series
-    mail(:to => ["jrpage@hawaii.edu", "btrevino@hawaii.edu", "james29@hawaii.edu"], :subject => subject) #{})
-      rescue => e
-        @mailer_error = e.message
-          mail(:to => ["btrevino@hawaii.edu", "jrpage@hawaii.edu"], :subject => "[UDAMACMINI] PackageMailer.website_post_notification")
-      end
+    begin
+      subject = "Udamacmini: New data for #{post_name} posted to the UHERO website" if created
+      subject = "Udamacmini tried but failed to post new data for #{post_name} to the UHERO website" unless created
+      @post_address = post_address
+      @new_data_series = new_data_series
+      mail(:to => ["jrpage@hawaii.edu", "btrevino@hawaii.edu", "james29@hawaii.edu"], :subject => subject) #{})
+    rescue => e
+      mail(:to => ["btrevino@hawaii.edu", "jrpage@hawaii.edu"], :subject => "[UDAMACMINI] PackageMailer.website_post_notification error", :body => e.message, :content_type => "text/plain")
+    end
   end
-  
+
   def prognoz_notification(recipients, send_edition)
     begin
-    path = "/Users/uhero/Documents/data/prognoz_export/ready_to_send_zip_files/"
-    filename = send_edition + ".zip"
-    attachments[filename] = File.read(path+filename) 
-    subject = "Prognoz Export (Udamacmini)"
-    mail(:to => recipients, :subject => subject)  
+      path = "/Users/uhero/Documents/data/prognoz_export/ready_to_send_zip_files/"
+      filename = send_edition + ".zip"
+      attachments[filename] = File.read(path+filename) 
+      subject = "Prognoz Export (Udamacmini)"
+      mail(:to => recipients, :subject => subject)  
     rescue => e
-        @mailer_error = e.message
-        mail(:to => ["btrevino@hawaii.edu", "jrpage@hawaii.edu"], :subject => "[UDAMACMINI] PackageMailer.prognoz_notification")
+      mail(:to => ["btrevino@hawaii.edu", "jrpage@hawaii.edu"], :subject => "[UDAMACMINI] PackageMailer.prognoz_notification error", :body => e.message, :content_type => "text/plain")
     end
   end
 end
