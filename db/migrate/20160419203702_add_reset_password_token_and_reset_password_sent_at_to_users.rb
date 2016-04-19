2805487b256b3ed8ed3f3ddc7828cdbc3dc66668
Jonathan Page <jonpage.econ@gmail.com>
2016-04-19 10:56:10 -1000

    fixing migration

--- db/migrate/20160419203702_add_reset_password_token_and_reset_password_sent_at_to_users.rb
+++ db/migrate/20160419203702_add_reset_password_token_and_reset_password_sent_at_to_users.rb
@@ -1,7 +1,7 @@
 class AddResetPasswordTokenAndResetPasswordSentAtToUsers < ActiveRecord::Migration
   def self.up
     add_column :users, :reset_password_token, :string
-    add_column :users, :reset_password_sent_at, :string
+    add_column :users, :reset_password_sent_at, :datetime
   end
 
   def self.down
