7c0e8b093c2c20bb28c291cb9112d580539413d3
Jonathan Page <jonpage.econ@gmail.com>
2016-04-19 10:57:41 -1000

    fixing migration. did not need to add reset_password_token

--- db/migrate/20160419203702_add_reset_password_token_and_reset_password_sent_at_to_users.rb
+++ db/migrate/20160419203702_add_reset_password_token_and_reset_password_sent_at_to_users.rb
@@ -1,11 +1,9 @@
 class AddResetPasswordTokenAndResetPasswordSentAtToUsers < ActiveRecord::Migration
   def self.up
-    add_column :users, :reset_password_token, :string
     add_column :users, :reset_password_sent_at, :datetime
   end
 
   def self.down
-    remove_column :users, :reset_password_token
     remove_column :users, :reset_password_sent_at
   end
 end
