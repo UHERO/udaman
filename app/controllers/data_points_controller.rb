00fd54c1dd896694a587994d3d17134e6cf40ea1
Jonathan Page <jonpage.econ@gmail.com>
2016-06-11 18:59:12 -1000

    refactored data_points controller for date

--- app/controllers/data_points_controller.rb
+++ app/controllers/data_points_controller.rb
@@ -1,9 +1,5 @@
 class DataPointsController < ApplicationController
   def show
-    @series = Series.find_by id: params[:series_id]
-    @as = AremosSeries.get @series.name
-    @desc = @as.nil? ? "No Aremos Series" : @as.description
-    @data_points = DataPoint.where(:series_id => params[:series_id], :date_string => params[:date_string])
-    @date_string = params[:date_string]
+    @data_points = DataPoint.where(:series_id => params[:series_id], :date => params[:date_string])
   end
 end
