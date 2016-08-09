9705cf15da5aa008de1b5854b28d265d2e7be3bf
Victoria Ward <vcward@hawaii.rr.com>
2016-08-09 11:20:44 -1000

    create categories table

---
+++ spec/views/categories/index.html.erb_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "categories/index", type: :view do
+  before(:each) do
+    assign(:categories, [
+      Category.create!(
+        :name => "Name",
+        :parent => 2,
+        :data_list_id => 3
+      ),
+      Category.create!(
+        :name => "Name",
+        :parent => 2,
+        :data_list_id => 3
+      )
+    ])
+  end
+
+  it "renders a list of categories" do
+    render
+    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => 2.to_s, :count => 2
+    assert_select "tr>td", :text => 3.to_s, :count => 2
+  end
+end
