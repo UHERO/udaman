a810dab098906ce9f7b94c392b616fa2d1669635
Jonathan Page <jonpage.econ@gmail.com>
2016-08-26 10:24:09 -1000

    fix for roo tmp files broke multithreading and AR

--- app/workers/series_worker.rb
+++ app/workers/series_worker.rb
@@ -4,7 +4,6 @@ class SeriesWorker
   include Sidekiq::Worker
   def perform(series_id)
     errors = Series.find(series_id).reload_sources
-    ObjectSpace.each_object(ActiveRecord::Relation).each(&:reset)
     GC.start
     unless errors.nil?
       File.open('public/reload_errors.log', 'a') {|f| f.puts errors }
