2329de8a7cec8484934768f7c3d8d5f5e7df7a08
Jonathan Page <jonpage.econ@gmail.com>
2016-09-23 16:58:58 -1000

    added ui to reorder categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,9 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:show, :edit, :update, :destroy]
+  before_action :set_category, only: [:show, :edit, :update, :destroy, :up, :down]
 
   # GET /categories
   def index
-    @category_roots = Category.roots
+    @category_roots = Category.roots.to_a
   end
 
   # GET /categories/1
@@ -43,6 +43,40 @@ class CategoriesController < ApplicationController
   def destroy
     @category.destroy
     redirect_to categories_url, notice: 'Category was successfully destroyed.'
+  end
+
+  def up
+    siblings_array = @category.siblings.to_a.sort_by{|sib| sib.order}
+    old_index = siblings_array.index(@category)
+    siblings_array.each_index do |i|
+      if old_index - 1 == i
+        siblings_array[i].update order: i + 1
+        next
+      end
+      if old_index == i
+        siblings_array[i].update order: i - 1
+        next
+      end
+      siblings_array[i].update order: i
+    end
+    redirect_to categories_url
+  end
+
+  def down
+    siblings_array = @category.siblings.to_a.sort_by{|sib| sib.order}
+    old_index = siblings_array.index(@category)
+    siblings_array.each_index do |i|
+      if old_index + 1 == i
+        siblings_array[i].update order: i - 1
+        next
+      end
+      if old_index == i
+        siblings_array[i].update order: i + 1
+        next
+      end
+      siblings_array[i].update order: i
+    end
+    redirect_to categories_url
   end
 
   private
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,21 +1,45 @@
 module CategoriesHelper
-  def show_table(root)
+  def show_table(root, first, last)
     if root.is_childless?
-      return "<li>#{show_list_item root}</li>"
+      return "<li>#{show_list_item(root, first, last)}</li>"
     end
-    '<li>' << show_list_item(root) << '<ul>' <<
-    root.children.map{|node| show_table(node)}.join('') <<
+
+    categories = (root.children.to_a).sort_by!{|cat| cat.order}
+    category_strings = []
+    categories.each_index{|i| category_strings.push(show_table(categories[i], i == 0, i + 1 == categories.length))}
+
+    '<li>' << show_list_item(root, first, last) << '<ul>' <<
+    category_strings.join('') <<
     '</ul></li>'
   end
 
-  def show_list_item(leaf)
+  def show_list_item(leaf, first, last)
     data_list_section = link_to('Add DataList', edit_category_path(leaf))
     unless leaf.data_list.nil?
       data_list_section = link_to(leaf.data_list.name, "data_lists/super_table/#{leaf.data_list_id}")
     end
-    "#{leaf.name} (" <<
+
+    "<strong>#{leaf.name}</strong> (" <<
     data_list_section <<
-    ') ' << link_to('Edit', edit_category_path(leaf)) << ' - ' <<
+    ') ' <<
+    order_section(leaf, first, last) << ' - ' <<
+    link_to('Edit', edit_category_path(leaf)) << ' - ' <<
     link_to('Destroy', leaf, method: :delete, data: { confirm: 'Are you sure?' })
   end
+
+  private
+    def order_section(leaf, first, last)
+      if first && last
+        return ''
+      end
+      if first
+        return link_to('Down', "/categories/down/#{leaf.id}")
+      end
+      if last
+        return link_to('Up', "/categories/up/#{leaf.id}")
+      end
+
+      link_to('Up', "/categories/up/#{leaf.id}") << ' - ' <<
+      link_to('Down', "/categories/down/#{leaf.id}")
+    end
 end
