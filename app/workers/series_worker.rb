89adffa1c0d4e8c94f9ecd7fefa5f9d571f700e4
Jonathan Page <jonpage.econ@gmail.com>
2016-10-17 10:58:08 -1000

    namespaced worker reloads, added newlines, using redis better with getset

--- app/workers/series_worker.rb
+++ app/workers/series_worker.rb
@@ -11,51 +11,49 @@ class SeriesWorker
     end
     # check to see if the queue is empty
     if Sidekiq::Queue.new.size > 0
-      puts 'WORKER: queue is not empty'
+      puts "\nWORKER (#{series_size}): queue is not empty\n\n"
       return
     end
-    puts 'WORKER: queue is empty'
+    puts "\nWORKER (#{series_size}): queue is empty\n\n"
     # if the queue is empty see if another job has raised the flag
     redis = Redis.new
-    if redis.get('finishing_depth') == 'true'
-      puts 'WORKER: Another worker will finish'
+    if redis.getset("finishing_depth_#{series_size}", 'true') == 'true'
+      puts "\nWORKER (#{series_size}): Another worker will finish\n\n"
       return
     end
-    redis.incr('waiting_workers')
-    puts 'WORKER: This worker will finish'
-    # no one else has so time to raise the flag
-    redis.set('finishing_depth', true)
+    redis.incr("waiting_workers_#{series_size}")
+    puts "\nWORKER (#{series_size}): This worker will finish\n\n"
     # wait for everyone else to finish
     while Sidekiq::Workers.new.size > 1
       sleep(1)
       # the random component helps avoid a race condition between two processes
-      if redis.get('waiting_workers').to_i > 1 && rand > 0.5
-        redis.decr('waiting_workers')
+      if redis.get("waiting_workers_#{series_size}").to_i > 1 && rand > 0.5
+        redis.decr("waiting_workers_#{series_size}")
         return
       end
     end
     # if no workers are busy, the queue should be filled with the next
-    next_depth = redis.get('current_depth').to_i - 1
+    redis.set("finishing_depth_#{series_size}", false)
+    redis.decr("waiting_workers_#{series_size}")
+    next_depth = redis.get("current_depth_#{series_size}").to_i - 1
     if next_depth == -1
-      puts 'WORKER: on last depth'
+      puts "\nWORKER (#{series_size}): on last depth\n\n"
       return
     end
-    puts "WORKER: Next depth: #{next_depth}"
-    redis.set('current_depth', next_depth)
+    puts "\nWORKER (#{series_size}): next depth: #{next_depth}\n\n"
+    redis.set("current_depth_#{series_size}", next_depth)
     series_ids = redis.get("series_list_#{series_size}").scan(/\d+/).map{|s| s.to_i}
     next_series = Series.all.where(:id => series_ids, :dependency_depth => next_depth)
     while next_series.count == 0
       next_depth -= 1
-      puts "WORKER: Next depth: #{next_depth}"
+      puts "\nWORKER (#{series_size}): next depth: #{next_depth}\n\n"
       return if next_depth == -1
-      redis.set('current_depth', next_depth)
+      redis.set("current_depth_#{series_size}", next_depth)
       next_series = Series.all.where(:id => series_ids, :dependency_depth => next_depth)
     end
     Series.all.where(:id => series_ids, :dependency_depth => next_depth).pluck(:id).each do |id|
       SeriesWorker.perform_async id, series_size
     end
-    puts 'WORKER: queued up the next depth'
-    redis.set('finishing_depth', false)
-    redis.decr('waiting_workers')
+    puts "\nWORKER (#{series_size}): queued up the next depth\n\n"
   end
 end
