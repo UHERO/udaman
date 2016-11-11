99ecbe16ebc2510d5584ead32c30e9d6d3be49d6
dji <David.Iannucci@hawaii.edu>
2016-11-10 17:08:29 -1000

    Migration for Data Lists: d'oh forgot I was making these IDs into Users

--- db/migrate/20161110011213_add_user_tracking_to_data_lists.rb
+++ db/migrate/20161110011213_add_user_tracking_to_data_lists.rb
@@ -1,9 +1,9 @@
 class AddUserTrackingToDataLists < ActiveRecord::Migration
   def change
     change_table :data_lists do |t|
-      t.string created_by
-      t.string updated_by
-      t.string owned_by
+      t.integer created_by
+      t.integer updated_by
+      t.integer owned_by
     end
   end
 end
