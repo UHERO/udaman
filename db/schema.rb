7071b6acb24267805396a554158e8dc5c16f463a
Jonathan Page <jonpage.econ@gmail.com>
2016-11-13 10:34:16 -1000

    added tcase function to db and copy aremos names to series table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161104013922) do
+ActiveRecord::Schema.define(version: 20161111023506) do
 
   create_table "api_applications", force: :cascade do |t|
     t.string   "name",            limit: 255
