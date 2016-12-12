6ea8b8b1f942850e0cf8dcc4da30d338179248fb
dji <David.Iannucci@hawaii.edu>
2016-12-12 11:45:53 -1000

    Shouldn't check authorization at controller parent class level

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
-  include Authorization
-
-  before_action :check_authorization
   before_filter :authenticate_user!
   
   helper :all # include all helpers, all the time
