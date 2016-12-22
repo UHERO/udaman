db02d16dff84ece76e4d363c4414a5104fee9ae8
Jonathan Page <jonpage.econ@gmail.com>
2016-12-21 16:00:10 -1000

    link to exports and removal of prognoz link in header

--- app/helpers/exports_helper.rb
+++ app/helpers/exports_helper.rb
@@ -10,4 +10,18 @@ module ExportsHelper
       end
     end
   end
+
+  def exports_google_charts_list
+    series_data = @export.series_data
+    sorted_names = series_data.keys.sort
+    dates_array = @export.data_dates
+    series_data_arrays = {}
+    sorted_names.each {|s| series_data_arrays[s] = dates_array.map {|date| series_data[s][date].to_s} }
+    rs = "data = new google.visualization.DataTable();\n" <<
+    "data.addColumn('string', 'date');\ndata.addColumn('number','" <<
+    sorted_names.join("');\n data.addColumn('number','") <<
+    "');\ndata.addRows(["
+    dates_array.each {|date| rs += "['"+ date.strftime('%Y-%m-%d') +"'," + sorted_names.map {|s| series_data[s][date].nil? ? 0 : series_data[s][date] }.join(', ') +"],\n"}
+    rs + "]);\n"
+  end
 end
