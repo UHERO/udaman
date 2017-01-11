847a9ebd08bf2fa2bb707ef7f9c891c6aa8e53fd
Victoria Ward <vcward@hawaii.rr.com>
2017-01-10 16:59:48 -1000

    get titles and units from measurements

--- app/models/forecast_snapshot.rb
+++ app/models/forecast_snapshot.rb
@@ -1,4 +1,21 @@
 class ForecastSnapshot < ActiveRecord::Base
   has_many :tsd_files
 
+  # Get series name from series mnemonic
+  def retrieve_name(prefix)
+    m = Measurement.find_by(prefix: prefix.chomp('NS'))
+    if m.nil?
+      return ''
+    end
+    m.data_portal_name
+  end
+
+  def retrieve_units(prefix)
+    m = Measurement.find_by(prefix: prefix.chomp('NS'))
+    if m.nil? || m.units_label_short.nil? || m.units_label_short == ''
+      return 'Values'
+    end
+    m.units_label_short
+  end
+
 end
