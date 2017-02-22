d0e7e575ef8b064327e847362a7827fb459b46ce
dji <David.Iannucci@hawaii.edu>
2017-02-22 11:46:39 -1000

    Had some stuff in wrong order

--- db/migrate/20170222040901_add_series_properties_to_measurements.rb
+++ db/migrate/20170222040901_add_series_properties_to_measurements.rb
@@ -1,11 +1,11 @@
 class AddSeriesPropertiesToMeasurements < ActiveRecord::Migration
   def change
-    add_column :measurements, :source_link, after: :real, :string
+    add_column :measurements, :source_link, :string, after: :real
     add_reference :measurements, :source, after: :real, foreign_key: true
     add_reference :measurements, :source_detail, after: :real, foreign_key: true
-    add_column :measurements, :seasonally_adjusted, after: :real, :boolean
-    add_column :measurements, :restricted, after: :real, :boolean
-    add_column :measurements, :units, :integer, after: :data_portal_name, limit: 4
-    add_column :measurements, :frequency_transform, :string, after: :notes
+    add_column :measurements, :seasonally_adjusted, :boolean, after: :real
+    add_column :measurements, :restricted, :boolean, after: :real
+    add_column :measurements, :units, :integer, limit: 4, after: :data_portal_name
+    add_column :measurements, :frequency_transform, :string, after: :data_portal_name
   end
 end
