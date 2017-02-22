ac0fc8ca68ac2d2c2f3794c0c3990510794de9b6
dji <David.Iannucci@hawaii.edu>
2017-02-22 11:34:36 -1000

    Added remaining column additions to migration

--- db/migrate/20170222040901_add_series_properties_to_measurements.rb
+++ db/migrate/20170222040901_add_series_properties_to_measurements.rb
@@ -1,7 +1,11 @@
 class AddSeriesPropertiesToMeasurements < ActiveRecord::Migration
   def change
-    add_reference :measurements, :source_detail, foreign_key: true
-    add_reference :measurements, :source, foreign_key: true
-    add_column :measurements, :source_link, :string
+    add_column :measurements, :source_link, after: :real, :string
+    add_reference :measurements, :source, after: :real, foreign_key: true
+    add_reference :measurements, :source_detail, after: :real, foreign_key: true
+    add_column :measurements, :seasonally_adjusted, after: :real, :boolean
+    add_column :measurements, :restricted, after: :real, :boolean
+    add_column :measurements, :units, :integer, after: :data_portal_name, limit: 4
+    add_column :measurements, :frequency_transform, :string, after: :notes
   end
 end
