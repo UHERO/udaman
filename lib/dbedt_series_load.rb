769d0c8babb7750a0dcd533c17d4a9be24442c81
dji <David.Iannucci@hawaii.edu>
2017-03-10 13:22:53 -1000

    wip

--- lib/dbedt_series_load.rb
+++ lib/dbedt_series_load.rb
@@ -1,27 +1,31 @@
 module DbedtSeriesLoad
   require 'csv'
 
-  def slurp_csv(path)
-    current_ind_id = 0
-    CSV.open(path, {col_sep: "\t", headers: true, return_headers: false}) do |row|
-      if current_ind_id == 0 || row[0].to_i != current_ind_id
-        current_ind_id = row[0].to_i
-        name = "DBEDT_#{current_ind_id}@#{get_geo_code(row[3])}.#{row[4]}"
+  def DbedtSeriesLoad.slurp_csv(path)
+    current_series = nil
+    CSV.foreach(path, {col_sep: "\t", headers: true, return_headers: false}) do |row|
+      name = 'DBEDT_'+row[0]+'@'+DbedtSeriesLoad.get_geo_code(row[3])+'.'+row[4]
+
+      if current_series.nil? || name != current_series
+        ## We've found beginning of next series, so create a new Series record
+        current_series = name
         this_series = Series.new(
             :name => name,
             :frequency => row[4],
             :description => row[1],
             :dataPortalName => row[1],
             :unitsLabel => row[8],
+            :source => Source.find_by(type: 'DBEDT', description: row[9]),
         )
         this_series.save  ## Check
       end
       ## add observation to current series as a data point. what about :current?
-      this_series.data_points.create(:date => get_date(row[5], row[6]), :value => row[7])
+      #puts ">>> >> New DP: date=#{DbedtSeriesLoad.get_date(row[5], row[6])}, value=#{row[7]}"
+      this_series.data_points.create(date: DbedtSeriesLoad.get_date(row[5], row[6]), value: row[7])
     end
   end
 
-  def get_geo_code(name)
+  def DbedtSeriesLoad.get_geo_code(name)
     trans_hash = {
       'Hawaii County' => 'HAW',
       'Honolulu County' => 'HON',
@@ -32,14 +36,14 @@ module DbedtSeriesLoad
     trans_hash[name] || 'ERROR'
   end
 
-  def get_date(year, qm)
-    if qm.empty? || qm == 'A'
-      "#{year}-01-01"
-    elsif qm =~ /^M(\d+)/
+  def DbedtSeriesLoad.get_date(year, qm)
+    if qm =~ /^M(\d+)/i
       "#{year}-#{$1}-01"
-    elsif qm =~ /^Q(\d+)/
-      quarter_month = '%02d' % ($1 - 1) * 3 + 1
+    elsif qm =~ /^Q(\d+)/i
+      quarter_month = '%02d' % (($1.to_i - 1) * 3 + 1)
       "#{year}-#{quarter_month}-01"
+    elsif qm.nil? || qm.empty? || qm =~ /A/i
+      "#{year}-01-01"
     else
       "#{year}-12-31"  ## use this as an error code? :=}
     end
