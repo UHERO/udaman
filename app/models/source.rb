38d47e739a1158a4537727b5baa56d1369c8e0ad
dji <David.Iannucci@hawaii.edu>
2017-03-10 17:18:10 -1000

    Made get_or_new_dbedt a class method

--- app/models/source.rb
+++ app/models/source.rb
@@ -2,8 +2,9 @@ class Source < ActiveRecord::Base
   has_many :series
   has_many :measurements
 
-  def get_or_new_dbedt(description)
-    Source.find_by(source_type: 'DBEDT', description: description) || Source.create(source_type: 'DBEDT', description: description)
+  def Source.get_or_new_dbedt(description)
+    Source.find_by(source_type: 'DBEDT', description: description) ||
+    Source.create(source_type: 'DBEDT', description: description)
   end
 
 end
