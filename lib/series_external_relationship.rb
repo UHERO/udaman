1cc2060a0d42a722132c91cf5b6df074aef95214
Jonathan Page <jonpage.econ@gmail.com>
2017-03-10 17:05:38 -1000

    Move quarantine check to data point updates (UA-495) (PR#252)

--- lib/series_external_relationship.rb
+++ lib/series_external_relationship.rb
@@ -72,9 +72,6 @@ module SeriesExternalRelationship
           #have to do all the rounding because it still seems to suffer some precision errors after initial rounding
           diff = a_diff(value, self.units_at(date))
           self.aremos_diff +=  diff
-          if diff != 0 && (Date.today - 2.years > date) && !quarantined && !restricted
-            self.update! quarantined: true
-          end
           puts "#{self.name}: #{date}: #{value}, #{self.units_at(date)} diff:#{diff}" if diff != 0
         end
       end
