e558c1d91923ad8b8af18af8cfefac75dcca26d0
dji <David.Iannucci@hawaii.edu>
2017-03-13 16:57:43 -1000

    Added starter JS code provided by jon

---
+++ app/assets/javascripts/dbedt_uploads.js
@@ -0,0 +1,27 @@
+$(function() {
+   $('.upload-status.processing').each(function(_, element) {
+       // This assumes you give each upload-status element an id like "upload-123" where 123 is the dbedt_uploads_id
+       var upload_id = $(element).attr('id').split('-')[1];
+       // Check once a second
+       var intervalId = setInterval(updateClass, 1000);
+       function updateClass() {
+           // TODO: send request to the right url
+           $.get('/dbedt_uploads/' + upload_id + '/status', function(data) {
+               // data should be the content you deliver from the status endpoint
+               if (data === 'processing') {
+                   // no update needed since the class is already set to processing
+                   return;
+               }
+               if (data === 'success') {
+                   $(element).removeClass('processing').addClass('success');
+                   clearInterval(intervalId);
+                   return;
+               }
+               if (data === 'failure') {
+                   $(element).removeClass('processing').addClass('failure');
+                   clearInterval(intervalId);
+               }
+           });
+       }
+   });
+});
