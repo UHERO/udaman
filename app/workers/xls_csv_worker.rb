1adc9c7f87f1559dd04e090a775a86df09716fd1
dji <David.Iannucci@hawaii.edu>
2017-03-15 17:25:08 -1000

    Put xlsx2csv into play

--- app/workers/xls_csv_worker.rb
+++ app/workers/xls_csv_worker.rb
@@ -9,12 +9,9 @@ class XlsCsvWorker
   def perform(dbu_id, which)
     dbu = DbedtUpload.find(dbu_id)
     abspath = (which == 'cats') ? dbu.cats_file_abspath : dbu.series_file_abspath
-    #if system "xlsx2csv -s 1 -d tab #{abspath} #{abspath.change_file_ext('csv')}"
-    if system "sleep 4; cp #{abspath} #{abspath.change_file_ext('csv')}"
-      puts ">>> cp WORKED"
+    if system "xlsx2csv -s 1 -d tab #{abspath} #{abspath.change_file_ext('csv')}"
       dbu.set_status(which, :ok)
     else
-      puts ">>> cp FAILED"
       dbu.set_status(which, :fail)
     end
   end
