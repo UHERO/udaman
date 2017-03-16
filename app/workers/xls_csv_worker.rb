d4d038774b13e35eafdb6f4dc19d05f4a9eba760
dji <David.Iannucci@hawaii.edu>
2017-03-15 18:30:23 -1000

    Fergot the durn .py at the end of command name :=(

--- app/workers/xls_csv_worker.rb
+++ app/workers/xls_csv_worker.rb
@@ -9,7 +9,7 @@ class XlsCsvWorker
   def perform(dbu_id, which)
     dbu = DbedtUpload.find(dbu_id)
     abspath = (which == 'cats') ? dbu.cats_file_abspath : dbu.series_file_abspath
-    if system "xlsx2csv -s 1 -d tab #{abspath} #{abspath.change_file_ext('csv')}"
+    if system "xlsx2csv.py -s 1 -d tab #{abspath} #{abspath.change_file_ext('csv')}"
       dbu.set_status(which, :ok)
     else
       dbu.set_status(which, :fail)
