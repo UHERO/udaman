e8a86f28b7095c59499a5b2965956e077287c03f
dji <David.Iannucci@hawaii.edu>
2017-03-17 11:47:12 -1000

    Some renaming and de-classifying

--- lib/dbedt_series_load.rb
+++ lib/dbedt_series_load.rb
@@ -1,11 +1,11 @@
-module DbedtSeriesLoad
+module DbedtLoadSeries
   require 'csv'
 
-  def DbedtSeriesLoad.slurp_csv(path)
+  def load_series_csv(path)
     current_series = nil
     CSV.foreach(path, {col_sep: "\t", headers: true, return_headers: false}) do |row|
       this_series = nil
-      name = 'DBEDT_' + row[0] + '@' + DbedtSeriesLoad.get_geo_code(row[3]) + '.' + row[4]
+      name = 'DBEDT_' + row[0] + '@' + get_geo_code(row[3]) + '.' + row[4]
 
       if current_series.nil? || name != current_series
         ## We've found beginning of next series, so create a new Series record
@@ -22,11 +22,11 @@ module DbedtSeriesLoad
       end
       ## add observation to current series as a data point. what about :current?
       #puts ">>> >> New DP: date=#{DbedtSeriesLoad.get_date(row[5], row[6])}, value=#{row[7]}"
-      this_series.data_points.create(date: DbedtSeriesLoad.get_date(row[5], row[6]), value: row[7])
+      this_series.data_points.create(date: get_date(row[5], row[6]), value: row[7])
     end
   end
 
-  def DbedtSeriesLoad.get_geo_code(name)
+  def get_geo_code(name)
     trans_hash = {
       'Hawaii County' => 'HAW',
       'Honolulu County' => 'HON',
@@ -37,7 +37,7 @@ module DbedtSeriesLoad
     trans_hash[name] || 'ERROR'
   end
 
-  def DbedtSeriesLoad.get_date(year, qm)
+  def get_date(year, qm)
     if qm =~ /^M(\d+)/i
       "#{year}-#{$1}-01"
     elsif qm =~ /^Q(\d+)/i
