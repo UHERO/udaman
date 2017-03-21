b9b9cc639a866e8b6a502e99e77380f420d46010
dji <David.Iannucci@hawaii.edu>
2017-03-21 09:24:11 -1000

    wip

--- app/workers/xls_csv_worker.rb
+++ app/workers/xls_csv_worker.rb
@@ -9,8 +9,14 @@ class XlsCsvWorker
   def perform(dbu_id, which)
     dbu = DbedtUpload.find(dbu_id)
     abspath = dbu.file_abspath(which)
-    if system "xlsx2csv.py -s 1 -d tab #{abspath} #{abspath.change_file_ext('csv')}"
+    csv_path = abspath.change_file_ext('csv')
+    if system "xlsx2csv.py -s 1 -d tab #{abspath} #{csv_path}"
       dbu.set_status(which, :ok)
+      if system "rsync -t #{csv_path} deploy@uhero-worker.colo.hawaii.edu:/data/dbedt_files"
+        #do something
+      else
+        #do somethingelse
+      end
     else
       dbu.set_status(which, :fail)
     end
