27e967e4d73e1e4108030d7f6e8b120cf1e70f5c
dji <David.Iannucci@hawaii.edu>
2017-03-21 17:01:38 -1000

    make migration reversible (yes?)

--- db/migrate/20170322000044_changes_to_cols_in_dbedt_uploads.rb
+++ db/migrate/20170322000044_changes_to_cols_in_dbedt_uploads.rb
@@ -1,6 +1,10 @@
 class ChangesToColsInDbedtUploads < ActiveRecord::Migration
-  def change
+  def self.up
     change_column :dbedt_uploads, :active, "ENUM('yes','no','loading','fail')", :default => 'no'
     remove_column :dbedt_uploads, :load_status if column_exists? :dbedt_uploads, :load_status
+  end
+  def self.down
+    change_column :dbedt_uploads, :active, :boolean
+    add_column :dbedt_uploads, :load_status, "ENUM('processing','ok','fail')", after: :series_status
   end
 end
