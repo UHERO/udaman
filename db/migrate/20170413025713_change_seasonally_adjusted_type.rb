2e7eda1da97f41f3a2f0dcf3322d09e86def18ee
dji <David.Iannucci@hawaii.edu>
2017-04-13 12:28:04 -1000

    Sigh - still tweaking to make things match

--- db/migrate/20170413025713_change_seasonally_adjusted_type.rb
+++ db/migrate/20170413025713_change_seasonally_adjusted_type.rb
@@ -1,6 +1,6 @@
 class ChangeSeasonallyAdjustedType < ActiveRecord::Migration
   def change
-    add_column :series, :seasonal_adjustment, %q(ENUM('Seasonally-adjusted', 'Not-seasonally-adjusted', 'not-applicable')), after: :seasonally_adjusted
-    add_column :measurements, :seasonal_adjustment, %q(ENUM('Seasonally-adjusted', 'Not-seasonally-adjusted', 'not-applicable')), after: :seasonally_adjusted
+    add_column :series, :seasonal_adjustment, %q(ENUM('Seasonally-adjusted', 'Non-seasonally-adjusted', 'Not-applicable')), after: :seasonally_adjusted
+    add_column :measurements, :seasonal_adjustment, %q(ENUM('Seasonally-adjusted', 'Non-seasonally-adjusted', 'Not-applicable')), after: :seasonally_adjusted
   end
 end
