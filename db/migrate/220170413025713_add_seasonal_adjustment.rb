5d6c591eb2e442dc5d07ddb109013b54ba788aa7
dji <David.Iannucci@hawaii.edu>
2017-04-13 13:33:32 -1000

    Renamed migration

--- db/migrate/220170413025713_add_seasonal_adjustment.rb
+++ db/migrate/220170413025713_add_seasonal_adjustment.rb
@@ -1,4 +1,4 @@
-class ChangeSeasonallyAdjustedType < ActiveRecord::Migration
+class AddSeasonalAdjustment < ActiveRecord::Migration
   def change
     add_column :series, :seasonal_adjustment, %q(ENUM('Seasonally-adjusted', 'Non-seasonally-adjusted', 'Not-applicable')), after: :seasonally_adjusted
     add_column :measurements, :seasonal_adjustment, %q(ENUM('Seasonally-adjusted', 'Non-seasonally-adjusted', 'Not-applicable')), after: :seasonally_adjusted
