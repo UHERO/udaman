fe594bc701725a2edf96212855df7147bcc71df9
dji <David.Iannucci@hawaii.edu>
2017-04-18 11:55:47 -1000

    Revert "new migration"
    This reverts commit 30eb255769fae12ef716b1a43d96c9576b522bce.

--- db/migrate/220170413025714_add_prefixes_and_decimals_to_measurements.rb
+++
@@ -1,8 +0,0 @@
-class AddPrefixesAndDecimalsToMeasurements < ActiveRecord::Migration
-  def change
-    add_column :measurements, :decimals, :integer, after: :real
-    add_column :series, :decimals, :integer, after: :real
-    add_column :measurements, :table_prefix, :string, after: :data_portal_name
-    add_column :measurements, :table_postfix, :string, after: :table_prefix
-  end
-end
