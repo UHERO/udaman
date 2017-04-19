5a516f16c3218ac5b3155fd1ca25cb20d60ed05c
dji <David.Iannucci@hawaii.edu>
2017-04-18 17:29:06 -1000

    wip

--- db/migrate/220170413025715_add_hidden_to_categories.rb
+++ db/migrate/220170413025715_add_hidden_to_categories.rb
@@ -1,5 +1,5 @@
 class AddHiddenToCategories < ActiveRecord::Migration
   def change
-    add_column :categories, :hidden, :boolean, after: :updated_at
+    add_column :categories, :hidden, :boolean, default: false, after: :updated_at
   end
 end
