f5a68f8116db8c5743ebb01f6aab31f454d4b46e
dji <David.Iannucci@hawaii.edu>
2017-04-27 13:08:53 -1000

    Schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 220170413025715) do
+ActiveRecord::Schema.define(version: 220170413025716) do
 
   create_table "api_applications", force: :cascade do |t|
     t.string   "name",            limit: 255
@@ -288,6 +288,14 @@ ActiveRecord::Schema.define(version: 220170413025715) do
     t.string   "output_start_date", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "public_data_points", id: false, force: :cascade do |t|
+    t.integer  "series_id",  limit: 4,  default: 0, null: false
+    t.date     "date",                              null: false
+    t.float    "value",      limit: 53
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
   end
 
   create_table "series", force: :cascade do |t|
