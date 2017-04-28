088dc40d7ede2b351d5d686cb5aeeb917b002822
kotodharma <kotodharma@users.noreply.github.com>
2017-04-27 16:39:40 -1000

    Fix migration number (PR#284)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 220170413025716) do
+ActiveRecord::Schema.define(version: 220170413025717) do
 
   create_table "api_applications", force: :cascade do |t|
     t.string   "name",            limit: 255
