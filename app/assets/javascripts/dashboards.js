9adf1b85f992d363101c8c7425483886120e8efe
Jonathan Page <jonpage.econ@gmail.com>
2017-05-10 10:52:37 -1000

    mild refactoring

--- app/assets/javascripts/dashboards.js
+++ app/assets/javascripts/dashboards.js
@@ -1,9 +1,8 @@
 $(function() {
-    $("form[data-remote],input[data-pdpupdate]").on("ajax:success", function(e, data, status, xhr) {
+    $(".button-with-indicator").on("ajax:success", function() {
         $("#pdp-indicator").removeClass('fa-refresh fa-spin').addClass('fa-check');
     });
-    $("#pdp-button").click(function(){
-        $("#pdp-indicator-span").show();
-        $("#pdp-indicator").removeClass('fa-check').addClass('fa-refresh fa-spin');
+    $(".button-with-indicator input").click(function(){
+        $("#pdp-indicator").show().removeClass('fa-check').addClass('fa-refresh fa-spin');
     });
 });
