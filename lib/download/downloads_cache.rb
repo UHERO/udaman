618abcfc0f2672bf60ae4633bd6989ec7e1244f9
dji <David.Iannucci@hawaii.edu>
2017-05-19 13:01:27 -1000

    Added a bunch of Rails.logger.debug instrumentation

--- lib/download/downloads_cache.rb
+++ lib/download/downloads_cache.rb
@@ -1,6 +1,7 @@
 class DownloadsCache
 
   def xls(handle, sheet, path = nil, date = nil)
+    Rails.logger.debug "... Entered method csv ... handle=#{handle}, sheet=#{sheet}, path=#{path}"
     if path.nil?
       @got_handle ||= {}
       @dsd = @got_handle[handle] || DataSourceDownload.get(handle)
@@ -17,6 +18,7 @@ class DownloadsCache
 
     #if handle in cache, it was downloaded recently... need to pull this handle logic out to make less hacky
     if handle != 'manual' && get_files_cache(file_key).nil?
+      Rails.logger.debug "!!! xls cache miss for file_key=#{file_key}"
       download_handle
       set_files_cache(file_key, 1)  ## Marker to show that file is downloaded
     end
@@ -24,6 +26,7 @@ class DownloadsCache
   end
 
   def set_xls_sheet(sheet, date)
+    Rails.logger.debug "... Entered method set_xls_sheet ... sheet=#{sheet}, date=#{date}"
     file_extension = @cache_handle.split('.')[-1]
     excel = file_extension == 'xlsx' ? Roo::Excelx.new(@cache_handle) : Roo::Excel.new(@cache_handle)
     sheet_parts = sheet.split(':')
@@ -50,11 +53,13 @@ class DownloadsCache
   end
 
   def download_results
+    Rails.logger.debug '... Entered method download_results'
     key = make_cache_key('download','results')
     get_files_cache(key) || {}
   end
 
   def csv(handle, path = nil)
+    Rails.logger.debug "... Entered method csv ... handle=#{handle}, path=#{path}"
     if path.nil?
       @got_handle ||= {}
       @dsd = @got_handle[handle] || DataSourceDownload.get(handle)
@@ -66,6 +71,7 @@ class DownloadsCache
     file_key = make_cache_key('csv', path)
     value = get_files_cache(file_key)
     if value.nil?
+      Rails.logger.debug "!!! csv cache miss for file_key=#{file_key}"
       download_handle unless @dsd.nil?
       begin
         value = CSV.read(path)
@@ -80,16 +86,17 @@ class DownloadsCache
   end
 
   def text(handle)
+    Rails.logger.debug "... Entered method text ... handle=#{handle}"
     @dsd = DataSourceDownload.get(handle)
     raise "handle '#{handle}' does not exist" if @dsd.nil?
-
     @handle = handle
-    key = make_cache_key('txt', handle)
-    value = get_files_cache(key)
+    file_key = make_cache_key('txt', handle)
+    value = get_files_cache(file_key)
     if value.nil?
+      Rails.logger.debug "!!! txt cache miss for file_key=#{file_key}"
       download_handle
       value = get_text_rows
-      set_files_cache(key, value)
+      set_files_cache(file_key, value)
     end
     value
   end
@@ -103,8 +110,8 @@ class DownloadsCache
     text_rows
   end
 
-private
   def download_handle
+    Rails.logger.debug "... Entered method download_handle ... @handle=#{@handle}"
     t = Time.now
     key = make_cache_key('download','results')
     results = get_files_cache(key) || {}
@@ -143,11 +150,13 @@ private
   end
 
   def get_files_cache(key)
+    Rails.logger.debug "<<< Entered method get_files_cache, key=#{key}"
     value = Rails.cache.fetch(key)
     value.nil? ? nil : Marshal.load(value)
   end
 
   def set_files_cache(key, value, options=nil)
+    Rails.logger.debug ">>> Entered method set_files_cache, key=#{key}"
     options ||= { expires_in: 6.hours }
     Rails.cache.write(key, Marshal.dump(value), options)
     value
