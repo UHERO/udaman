a7725349b20880fd5ffdc9062cd4e31cf0f15588
Jonathan Page <jonpage.econ@gmail.com>
2017-05-23 11:31:09 -1000

    missing close paren

--- app/models/series.rb
+++ app/models/series.rb
@@ -34,7 +34,7 @@ class Series < ActiveRecord::Base
                               not_seas_adj: 'not_seasonally_adjusted' }
 
   after_create do
-    self.update frequency: (Series.frequency_from_code(self.name.split('.').pop || self.frequency)
+    self.update frequency: (Series.frequency_from_code(self.name.split('.').pop) || self.frequency)
   end
 
   def as_json(options = {})
