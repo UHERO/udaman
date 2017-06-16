f6a2c284c415640db146bedca98a3ea7feb6d6cc
dji <David.Iannucci@hawaii.edu>
2017-06-15 15:14:17 -1000

    Add DataSourceDownload.get_or_new() method

--- app/models/data_source_download.rb
+++ app/models/data_source_download.rb
@@ -1,4 +1,10 @@
 class DataSourceDownload < ActiveRecord::Base
   belongs_to :data_source
   belongs_to :download
+
+  def DataSourceDownload.get_or_new(ds_id, dl_id)
+    DataSourceDownload.find_by(data_source_id: ds_id, download_id: dl_id) ||
+    DataSourceDownload.create(data_source_id: ds_id, download_id: dl_id)
+  end
+
 end
