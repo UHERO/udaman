30dc85b4b80b84dc80e26fc040715bf5781b8abf
dji <David.Iannucci@hawaii.edu>
2017-06-15 15:12:50 -1000

    Bringing migration up to date

---
+++ db/migrate/220170413025721_data_source_downloads_major_model_refactor.rb
@@ -0,0 +1,22 @@
+class DataSourceDownloadsMajorModelRefactor < ActiveRecord::Migration
+  def self.up
+    rename_table :data_source_downloads, :downloads
+    add_column :downloads, :last_download_at, :datetime, after: :updated_at
+    add_column :downloads, :last_change_at, :datetime, after: :last_download_at
+    rename_column :dsd_log_entries, :data_source_download_id, :download_id
+    create_table :data_source_downloads do |t|
+      t.belongs_to :data_source
+      t.belongs_to :download
+      t.datetime :last_file_vers_used, null: false, default: Time.at(0)
+      t.string :last_eval_options_used
+    end
+    add_index :data_source_downloads, [:data_source_id, :download_id], unique: true
+  end
+  def self.down
+    drop_table :data_source_downloads
+    remove_column :downloads, :last_download_at, :datetime
+    remove_column :downloads, :last_change_at, :datetime
+    rename_table :downloads, :data_source_downloads
+    rename_column :dsd_log_entries, :download_id, :data_source_download_id
+  end
+end
