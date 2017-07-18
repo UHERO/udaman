fe2f07509bea626fc9164778a7b9efa275d4e810
kotodharma <kotodharma@users.noreply.github.com>
2017-07-18 11:09:06 -1000

    Create NTA upload worker (UA-655)

---
+++ db/migrate/220170413025726_some_misc_changes.rb
@@ -0,0 +1,13 @@
+class SomeMiscChanges < ActiveRecord::Migration
+  def change
+    add_column :api_applications, :universe, %q(ENUM('UHERO', 'DBEDT', 'NTA')), null: false, default: 'UHERO', after: :id
+    add_index :api_applications, [:universe, :name], :unique => true
+    add_column :geographies, :region, :string, after: :handle
+    add_column :geographies, :subregion, :string, after: :region
+    add_column :geographies, :incgrp2015, :string, after: :subregion
+    add_index :geographies, [:universe, :handle, :incgrp2015], :unique => true
+    add_column :units, :universe, %q(ENUM('UHERO', 'DBEDT', 'NTA')), null: false, default: 'UHERO', after: :id
+    remove_index :units, [:short_label, :long_label]
+    add_index :units, [:universe, :short_label, :long_label], :unique => true
+  end
+end
