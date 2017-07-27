a391bd68ca965a0a3e0fd6b0ecc8b7d398b2fee0
dji <David.Iannucci@hawaii.edu>
2017-07-26 18:47:01 -1000

    Final changes to migration

--- db/migrate/220170413025727_create_geo_tree.rb
+++ db/migrate/220170413025727_create_geo_tree.rb
@@ -6,8 +6,9 @@ class CreateGeoTree < ActiveRecord::Migration
     end
     add_foreign_key :geo_trees, :geographies, column: :parent_id
     add_foreign_key :geo_trees, :geographies, column: :child_id
-#####    rename_column :geographies, :region, :geotype
-#####    add_reference :series, :geography, foreign_key: true, after: :dataPortalName
-#####    remove_index :geographies, [:universe, :handle, :incgrp2015]
+    rename_column :geographies, :region, :geotype
+    remove_index :geographies, [:universe, :handle, :incgrp2015]
+    add_index :geographies, [:universe, :handle], :unique => true
+    add_reference :series, :geography, foreign_key: true, after: :dataPortalName
   end
 end
