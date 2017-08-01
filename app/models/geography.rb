12c1b16bc8a227b8364d314399ff0f0e2ff8648f
kotodharma <kotodharma@users.noreply.github.com>
2017-08-01 10:15:17 -1000

    Expansion of NTA data structures (UA-680)

--- app/models/geography.rb
+++ app/models/geography.rb
@@ -2,6 +2,14 @@ class Geography < ActiveRecord::Base
 
   def Geography.get_or_new_nta(attrs, add_attrs = {})
     attrs.merge!(universe: 'NTA')
-    Geography.find_by(attrs) || Geography.create(attrs.merge(add_attrs))
+    geo = Geography.find_by(attrs)
+    unless geo
+      parents = add_attrs[:parents] ? [add_attrs.delete(:parents)].flatten : []
+      geo = Geography.create(attrs.merge(add_attrs))
+      unless parents.empty?
+        parents.each {|pid| GeoTree.create(parent_id: pid, child_id: geo.id) }
+      end
+    end
+    geo
   end
 end
