9d5bdf652d5f3b24e589d611ef494c50d7f424ea
Jonathan Page <jonpage.econ@gmail.com>
2017-09-29 08:22:13 -1000

    annual working, quarterly broken

--- app/models/tsd_file.rb
+++ app/models/tsd_file.rb
@@ -62,6 +62,11 @@ class TsdFile < ActiveRecord::Base
     name_array
   end
 
+  def get_current_plus_five_dates
+    dates = get_all_dates
+    dates.slice(dates.index{ |date| date > Date.today.to_s } - 2, 6)
+  end
+
   def get_all_dates
     dates = []
     get_all_series.each do |s|
