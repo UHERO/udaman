90dd7f8bf04ede4a2cb202315d8ba6fcefd5b432
dji <David.Iannucci@hawaii.edu>
2017-11-15 11:36:42 -1000

    wip

--- script/rename_downloads.rb
+++ script/rename_downloads.rb
@@ -1,6 +1,9 @@
 Download.all.each do |d|
+  spo = d.save_path_obsolete.dup
+  spo.sub!(ENV['DEFAULT_DATA_PATH'], ENV['DATA_PATH'])
+  #pathbase = File.basename(spo)
   begin
-    File.rename(d.save_path, d.new_save_path)
+    File.rename(spo, d.save_path)
   rescue => e
     Rails.logger.error e.message
   end
