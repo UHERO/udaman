51f4f604c94aa9bdc3c04d2363808216f1999021
dji <David.Iannucci@hawaii.edu>
2017-11-15 17:26:24 -1000

    wip

--- script/rename_downloads.rb
+++ script/rename_downloads.rb
@@ -4,6 +4,14 @@ Download.all.each do |d|
   #pathbase = File.basename(spo)
   begin
     File.rename(spo, d.save_path)
+    fte = d.file_to_extract.dup
+    unless fte.blank?
+      fte.sub!(ENV['DEFAULT_DATA_PATH'], ENV['DATA_PATH'])
+      ftepath = File.dirname(fte)
+      ftebase = File.basename(fte)
+      File.rename(ftepath, what?)
+      d.update_attributes(file_to_extract: ftebase)
+    end
   rescue => e
     Rails.logger.error e.message
   end
