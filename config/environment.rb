460eb00451a4b9e9022afa1192370279bb81cdac
kotodharma <kotodharma@users.noreply.github.com>
2017-12-12 16:36:49 -1000

    Move BEA and FRED api keys to environment variables (UA-722)

--- config/environment.rb
+++ config/environment.rb
@@ -9,3 +9,5 @@ ENV['LOAD_UPDATE_SPREADSHEET_PATTERNS_TO_DB'] = 'false'
 # Sets the default environment variables
 ENV['DEFAULT_DATA_PATH'] = '/Users/uhero/Documents/data'
 ENV['DATA_PATH'] ||= ENV['DEFAULT_DATA_PATH']
+# API_KEY_BEA, API_KEY_FRED must be provided by the outer environment if
+# those APIs are to be used.
--- lib/data_html_parser.rb
+++ lib/data_html_parser.rb
@@ -1,8 +1,9 @@
 class DataHtmlParser
 
   def get_fred_series(code, frequency = nil, aggregation_method = nil)
-    api_key = '1030292ef115ba08c1778a606eb7a6cc'
-    @url = "http://api.stlouisfed.org/fred/series/observations?series_id=#{code}&api_key=#{api_key}"
+    api_key = ENV['API_KEY_FRED']
+    raise 'No API key defined for FRED' unless api_key
+    @url = "http://api.stlouisfed.org/fred/series/observations?api_key=#{api_key}&series_id=#{code}"
     # frequencies: d, w, bw, m, q, sa, a (udaman represents semiannual frequency with S)
     @url += "&frequency=#{frequency.downcase.gsub(/^s$/, 'sa')}" unless frequency.nil?
     # avg, sum, eop
@@ -43,9 +44,10 @@ class DataHtmlParser
   # NIPA Test
   # http://www.bea.gov/api/data/?&UserID=66533E32-0B70-4EF6-B367-05662C3B7CA8&method=GetData&datasetname=NIPA&TableID=6&Frequency=A&Year=X&GeoFIPS=15001&ResultFormat=JSON&
   def get_bea_series(dataset, parameters)
-    api_key = '66533E32-0B70-4EF6-B367-05662C3B7CA8'
+    api_key = ENV['API_KEY_BEA']
+    raise 'No API key defined for BEA' unless api_key
     query_pars = parameters.map{|k, v| "#{k}=#{v}"}.join('&')
-    @url = "http://www.bea.gov/api/data/?&UserID=#{api_key}&method=GetData&datasetname=#{dataset}&#{query_pars}&ResultFormat=JSON&"
+    @url = "http://www.bea.gov/api/data/?UserID=#{api_key}&method=GetData&datasetname=#{dataset}&#{query_pars}&ResultFormat=JSON&"
     @doc = self.download
     new_data = {}
     bea_data = JSON.parse self.content
@@ -114,29 +116,25 @@ class DataHtmlParser
   end
   
   def download
-    begin
-      require 'uri'
-      require 'net/http'
-      require 'timeout'
-      
-      url = URI(@url)
-      
-      http = Net::HTTP.new(url.host, url.port)
-      http.use_ssl = url.scheme == 'https'
-      http.ssl_timeout = 60
-      if @post_parameters.nil? or @post_parameters.length == 0
-        @content = fetch(@url).read_body
-      else
-        request = Net::HTTP::Post.new(url)
-        request['cache-control'] = 'no-cache'
-        request['content-type'] = 'application/x-www-form-urlencoded'
-        request.body = URI::encode_www_form @post_parameters
-        @content = http.request(request).read_body
-      end
-      return Nokogiri::HTML(@content)
-    rescue Exception
-      return 'Something went wrong with download'
+    require 'uri'
+    require 'net/http'
+    require 'timeout'
+
+    url = URI(@url)
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = url.scheme == 'https'
+    http.ssl_timeout = 60
+    if @post_parameters.nil? or @post_parameters.length == 0
+      @content = fetch(@url).read_body
+    else
+      request = Net::HTTP::Post.new(url)
+      request['cache-control'] = 'no-cache'
+      request['content-type'] = 'application/x-www-form-urlencoded'
+      request.body = URI::encode_www_form @post_parameters
+      @content = http.request(request).read_body
     end
+    return Nokogiri::HTML(@content)
   end
 
   def fetch(uri_str, limit = 10)
