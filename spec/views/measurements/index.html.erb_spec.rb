8452adf59f90f3695bd8550f0a57e695bff0620a
dji <David.Iannucci@hawaii.edu>
2018-01-03 18:17:12 -1000

    another attempt to fix test

--- spec/views/measurements/index.html.erb_spec.rb
+++ spec/views/measurements/index.html.erb_spec.rb
@@ -2,11 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'measurements/index', :type => :view do
   before(:each) do
+    assign(:units, [
+        Unit.create!(
+                long_label: 'Thousands of Widgets',
+                short_label: 'WidgThous'
+        )
+    ])
     assign(:measurements, [
       Measurement.create!(
         :prefix => 'Prefix',
         :data_portal_name => 'Data Portal Name',
-        :unit_id => 42,
+        :unit_id => Unit.first.id,
         :percent => false,
         :real => true,
         :notes => 'MyText'
@@ -14,7 +20,7 @@ RSpec.describe 'measurements/index', :type => :view do
       Measurement.create!(
         :prefix => 'Prefix',
         :data_portal_name => 'Data Portal Name',
-        :unit_id => 42,
+        :unit_id => Unit.first.id,
         :percent => false,
         :real => true,
         :notes => 'MyText'
@@ -26,7 +32,7 @@ RSpec.describe 'measurements/index', :type => :view do
     render
     assert_select 'tr>td', :text => 'Prefix'.to_s, :count => 2
     assert_select 'tr>td', :text => 'Data Portal Name'.to_s, :count => 2
-    assert_select 'tr>td', :text => 42.to_s, :count => 2
+    assert_select 'tr>td', :text => Unit.first.id.to_s, :count => 2
     assert_select 'tr>td', :text => false.to_s, :count => 2
     assert_select 'tr>td', :text => true.to_s, :count => 2
     assert_select 'tr>td', :text => 'MyText'.to_s, :count => 2
