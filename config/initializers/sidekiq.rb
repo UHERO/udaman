aae21b3d2f3b908f7b219cbfeb8b34a274e3be8c
dji <David.Iannucci@hawaii.edu>
2018-03-29 10:19:30 -1000

    wip

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 Sidekiq.configure_server do |config|
-  ## All Rails logging should go to the Sidekiq logger when code is running in Sidekiq
+  ## All Rails logging should go to the Sidekiq logger when (and only when) code is running in Sidekiq
   Rails.logger = Sidekiq::Logging.logger
-
+  ActiveRecord::Base.logger = Sidekiq::Logging.logger
 end
