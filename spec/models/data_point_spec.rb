c683af1a664ffa78427c0dcebababcd22a2697ee
dji <David.Iannucci@hawaii.edu>
2018-04-27 14:24:43 -1000

    wip

--- spec/models/data_point_spec.rb
+++ spec/models/data_point_spec.rb
@@ -8,35 +8,53 @@ describe DataPoint do
   #redo tests above without the update function
   it 'should not change data_point if value and source_id are unchanged' do
     ds = DataSource.create
-    dp = DataPoint.create(:series_id => @s.id, :date => '2011-03-01', :value => 100.0, :data_source_id => ds.id, :current => true)
+    dp = DataPoint.create(id: 99,  ## anything other than zero
+                          :series_id => @s.id, :date => '2011-03-01', :value => 100.0, :data_source_id => ds.id, :current => true)
     newdp = dp.upd(100, ds)
+    cur_dps = @s.current_data_points
+
     dpu = @s.current_data_points.first
+    expect(cur_dps.count).to eq(1), 'not exactly one current dp'
     expect(newdp).to eq(nil)
     expect(dpu.same_value_as? dp.value).to eq(true), 'not the same value'
     expect(dpu.current).to eq(true)
     expect(dpu.data_source_id).to eq(dp.data_source_id)
   end
   
-  it 'should update a data_points data source if source is different and its priority is not less' do
+  it 'should update a data_points data source if source is different, source.priority not less than current' do
     ds1 = DataSource.create priority: 80
     ds2 = DataSource.create priority: 80
-    dp = DataPoint.create(:series_id => @s.id, :date => '2011-03-01', :value => 100.0, :data_source_id => ds1.id, :current => true)
+    arbitrary_id = 99  ## anything other than zero
+    dp = DataPoint.create(id: arbitrary_id,
+                          :series_id => @s.id, :date => '2011-03-01', :value => 100.0, :data_source_id => ds1.id, :current => true)
     newdp = dp.upd(100, ds2)
-    
     cur_dps = @s.current_data_points
-    expect(cur_dps.count).to eq(1), 'more than one current dp'
+
+    expect(cur_dps.count).to eq(1), 'not exactly one current dp'
     expect(newdp.class).to eq(DataPoint), 'thing returned is not a DataPoint'
-    expect(newdp.created_at).to eq(cur_dps.first.created_at), '(seems) a new dp was not created'
-    expect(newdp.current).to eq(true), 'new data point not set to current'
-
+    expect(newdp.id).not_to eq(arbitrary_id), 'a new dp was not created'
+    expect(newdp.id).to eq(cur_dps.first.id), 'new dp not returned by current_data_points'
+    expect(newdp.current).to eq(true), 'new dp not set to current'
+    expect(dp.current).to eq(false), 'old dp still current'
     expect(newdp.same_value_as? dp.value).to eq(true), 'dp values are not equal'
-    expect(dp.current).to eq(false), 'old data point still current'
-
-    expect(newdp.data_source_id).to eq(newdp.id), ''
-    expect(newdp.data_source_id).to eq(ds2.id), 'ds2 does not have the same id'
-    expect(newdp.data_source_id).not_to eq(ds1.id), 'ds1 does have the same id'
+    expect(newdp.data_source_id).to eq(ds2.id), 'new dp does not have new source'
   end
-  
+
+  it 'should NOT update a data_points data source if source is different, source.priority less than current' do
+    ds1 = DataSource.create priority: 80
+    ds2 = DataSource.create priority: 70
+    arbitrary_id = 99  ## anything other than zero
+    dp = DataPoint.create(id: arbitrary_id,
+                          :series_id => @s.id, :date => '2011-03-01', :value => 100.0, :data_source_id => ds1.id, :current => true)
+    newdp = dp.upd(100, ds2)
+    cur_dps = @s.current_data_points
+
+    expect(cur_dps.count).to eq(1), 'not exactly one current dp'
+    expect(newdp.class).to eq(nil), 'thing returned is not nil'
+    expect(cur_dps.first.id).to eq(arbitrary_id), 'current dp is a different from original'
+    expect(dp.current).to eq(true), 'old data point no longer current'
+  end
+
   # it 'should be able to clone itself but assign a new value, source_id' do
   # end
   
