880c7331faf5b64d3b9da355644fba88a7042fcd
dji <David.Iannucci@hawaii.edu>
2018-05-01 14:20:07 -1000

    wip

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,11 @@
 Sidekiq.configure_server do |config|
   ## All Rails logging should go to the Sidekiq logger when code is running in Sidekiq
+  # but this doesn't seem to be working as expected at present :(
   Rails.logger = Sidekiq::Logging.logger
 
+  config.redis = { url: ENV['REDIS_SIDEKIQ_URL'] }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV['REDIS_SIDEKIQ_URL'] }
 end
