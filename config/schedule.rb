0d377f33ad1339918b4271857fd4e73e44605585
kotodharma <kotodharma@users.noreply.github.com>
2018-05-10 16:33:28 -1000

    Task to try reloading stale sources after nightly finishes (UA-918)

--- config/schedule.rb
+++ config/schedule.rb
@@ -53,6 +53,10 @@ every 1.day, :at => '9:10 pm' do
   rake 'reload_all_series'
 end
 
+every 1.day, :at => '9:10 am' do ## n.b. AM not PM
+  rake 'reload_recent_stale_series'
+end
+
 every 30.minutes do
   rake 'check_for_stalled_reload'
 end
