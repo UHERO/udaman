2aaf8ba3f9e509a39085362618a9339c5135afe4
dji <David.Iannucci@hawaii.edu>
2018-05-11 14:45:59 -1000

    wip

--- app/models/data_point.rb
+++ app/models/data_point.rb
@@ -68,10 +68,10 @@ class DataPoint < ActiveRecord::Base
 
   def make_current(dp)
     return unless current
-    self.transaction do
-      self.update_attributes({current: false})
-        dp.update_attributes({current: true})
-    end
+#    self.transaction do
+      self.update_attributes(current: false)
+        dp.update_attributes(current: true)
+#    end
   end
 
   def delete
