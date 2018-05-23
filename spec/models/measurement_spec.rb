6720c2d7c87a7b46433262a949e6464f130f8e32
dji <David.Iannucci@hawaii.edu>
2018-05-22 16:25:52 -1000

    added rspec tests

--- spec/models/measurement_spec.rb
+++ spec/models/measurement_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Measurement, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should clean up excess whitespace from strings saved to attributes on create' do
+    meas = Measurement.new(prefix: 'TEST1', data_portal_name: '', notes: '  ', table_prefix: ' FOO  ')
+    meas.save!
+
+    expect(meas.data_portal_name).to eq(nil), 'attribute set to empty string is not nullified'
+    expect(meas.notes).to eq(nil), 'attribute set to whitespace string is not nullified'
+    expect(meas.table_prefix).to eq('FOO'), 'attribute set to string with leading/trailing white is not stripped'
+  end
+
+  it 'should clean up excess whitespace from strings saved to attributes on update' do
+    meas = Measurement.new(prefix: 'TEST1', data_portal_name: 'DPN', notes: 'NOTES', table_prefix: 'PREFIX')
+    meas.save!
+    meas.update!(data_portal_name: '', notes: '  ', table_prefix: ' PREFIX  ')
+
+    expect(meas.data_portal_name).to eq(nil), 'attribute set to empty string is not nullified'
+    expect(meas.notes).to eq(nil), 'attribute set to whitespace string is not nullified'
+    expect(meas.table_prefix).to eq('PREFIX'), 'attribute set to string with leading/trailing white is not stripped'
+  end
 end
