5496c04e332c5bb651538313b6544633c414a7b7
dji <David.Iannucci@hawaii.edu>
2018-05-31 16:21:56 -1000

    wip

--- app/workers/series_slave_worker.rb
+++ app/workers/series_slave_worker.rb
@@ -14,7 +14,7 @@ class SeriesSlaveWorker
     @batch_id = batch_id
     @series = series_id
     begin
-      series = Series.find(series_id)
+      series = Series.find(series_id) rescue nil
       errors = []
       if series
         @series = "#{series.name} (#{series_id})"
@@ -22,24 +22,29 @@ class SeriesSlaveWorker
         errors = series.reload_sources(true)
       else
         mylogger :warn, 'no such series found'
+        slavelog_update(series_id, 'no such series found')
         errors.push 'no such series found'
       end
       if errors.empty?
         mylogger :info, 'reload SUCCEEDED'
-        log_update(batch_id, series_id, 'succeeded')
+        slavelog_update(series_id, 'succeeded')
       else
         mylogger :warn, 'reload ERRORED: check reload_errors.log'
-        log_update(batch_id, series_id, 'errored')
         File.open('public/reload_errors.log', 'a') {|f| f.puts errors }
       end
     rescue Exception => e
         mylogger :error, "exception caught: #{e.message}, backtrace follows:\n#{e.backtrace}"
+        slavelog_update(series_id, "rescued: #{e.message}")
     end
   end
 
 private
-  def log_update(batch_id, series_id, message)
+  def slavelog_update(series_id, message)
     log = SeriesSlaveLog.find_by(batch_id: @batch_id, series_id: series_id)
+    unless log
+      mylogger :error, 'Cannot find slavelog for this series!!!'
+      return
+    end
     log.update_attributes(message: message)
   end
 
