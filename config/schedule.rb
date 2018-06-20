918cdd047f01a338cae0c59f457f0835a43150d5
dji <David.Iannucci@hawaii.edu>
2018-06-20 12:15:23 -1000

    clean up the schedule code; change strings to symbols for easier IDE nav

--- config/schedule.rb
+++ config/schedule.rb
@@ -27,170 +27,74 @@ set :output, {:standard => '~/Documents/cronlog/udaman-download.log', :error => 
 job_type :rake,    "cd :path && #{%Q|DATA_PATH=#{ENV['DATA_PATH']}| unless ENV['DATA_PATH'].nil?} #{%Q|RAILS_ENV=#{ENV['RAILS_ENV']}| unless ENV['RAILS_ENV'].nil?} bundle exec rake :task"
 #job_type :rake,    "cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
 
-# this is a useful short task for testing cron
-# every 5.minutes do
-#     rake "test_case"
-# end
-
 every 1.day, :at => '11:00 am' do
-  rake 'reset_dependency_depth'
+  rake :reset_dependency_depth
 end
 
 every 1.day, :at => "4:00 pm" do
-  rake 'update_bea_links'
+  rake :update_bea_links
 end
 
 every 1.day, :at => '8:00 pm' do
-  rake 'reload_aremos'
+  rake :reload_aremos
 end
 
 every 1.day, :at => '9:00 pm' do
-  rake 'update_seats_links'
-  rake 'update_vis_history_links'
+  rake :update_seats_links
+  rake :update_vis_history_links
 end
 
 ## The famous "Nightly Reload"
 every 1.day, :at => '8:10 pm' do
-  rake 'batch_reload_uhero'
+  rake :batch_reload_uhero
 end
 
 every 1.day, :at => '8:10 am' do ## n.b. AM not PM
-  rake 'batch_reload_recent_stales'
+  rake :batch_reload_recent_stales
 end
 
 # ------- These two are together. Need only sometimes --------
 every 1.day, :at => '5:30 am' do
-  rake 'update_bea_links'
+  rake :update_bea_links
 end
 
 every 1.day, :at => '6:00 am' do
-  rake 'reload_bea_series_only'
-  rake 'reload_bls_series_only'
+  ## Note that these tasks take a long time to appear in sidekiq because of the very
+  ## long time needed to gather up their series in Series.get_all_series_by_eval
+  rake :reload_bea_series_only
+  rake :reload_bls_series_only
 end
 # -----------------------------------------------------------
 
 every 1.day, :at => "#{bls_hour.to_i+2}:30 am" do
-  rake 'write_ur_dash'
+  rake :write_ur_dash
 end
 
 every 1.day, :at => "#{hour.to_i+5}:30 am" do
-  rake 'gen_investigate_csv'
+  rake :gen_investigate_csv
 end
 
 every 1.day, :at => "#{hour.to_i+5}:50 am" do
-  rake 'aremos_exports'
+  rake :aremos_exports
 end
 
 every 1.day, :at => "#{hour.to_i+5}:55 am" do
-  rake 'tsd_exports'
+  rake :tsd_exports
 end
 
 every 1.day, :at => "#{hour.to_i+6}:00 am" do
-  rake 'update_public_data_points'
+  rake :update_public_data_points
 end
 
 every 1.day, :at => "#{hour.to_i+7}:00 am" do
-  rake 'categories_backup'
+  rake :categories_backup
 end
 
 every :saturday, :at => '9am' do
-  rake 'mark_pseudo_history'
+  rake :mark_pseudo_history
 end
 
 #bring down pv file daily
 every 1.day, :at => '9:15 am' do
   runner 'Download.get(\'PV_HON@hawaii.gov\').download'
 end
-
-#alternatives in case of emergency
-
-# every 1.day, :at => "12:05 pm" do
-#   rake "update_bea_links"
-# end
-# 
-# every 1.day, :at => "12:05 pm" do
-#   rake "jp_upd_a"
-#   rake "jp_upd_q"
-#   rake "jp_upd_m"
-# end
-# 
-# every 1.day, :at => "12:10 pm" do
-#   rake "tour_PC_upd"
-#   rake "tour_seats_upd"
-#   rake "tour_upd"
-#   rake "tour_ocup_upd"
-# end
-# 
-# every 1.day, :at => "12:30 pm" do
-#   rake "uic_upd"
-#   rake "const_upd_q"
-#   rake "const_upd_m"
-# end
-# 
-# every 1.day, :at => "12:35 pm" do
-#   rake "tax_upd"
-# end
-# 
-# every 1.day, :at => "12:40 pm" do
-#   rake "tour_rev_upd"
-# end
-# 
-# every 1.day, :at => "1:00 pm" do
-#   rake "tax_identities"
-# end
-# 
-# 
-# every 1.day, :at => "1:15 pm" do
-#   rake "gsp_upd" 
-#   rake "inc_upd_q"
-#   rake "inc_upd_a"
-#   rake "com_upd"
-# end
-# 
-# every 1.day, :at => "1:40 pm" do
-#   rake "us_upd_a"
-#   rake "us_upd_q"
-#   rake "us_upd_m"
-# end
-# 
-# every 1.day, :at => "2:00 pm" do 
-#   rake "bls_cpi_upd_m"
-#   rake "bls_job_upd_m"
-#   rake "bls_cpi_upd_s"
-#   rake "hiwi_upd"
-# end
-# 
-# every 1.day, :at => "2:30 pm" do 
-#   rake "bls_identities"
-#   rake "bea_identities"
-# end
-# 
-# every 1.day, :at => "3:00 pm" do
-#   rake "expenditures_and_nbi"
-#   rake "visitor_identities"
-#   rake "const_identities"
-# end
-# 
-# 
-# every 1.day, :at => "3:20 pm" do
-#   rake "run_aggregations"
-#   rake "run_aggregations2"
-#   rake "run_aggregations3"
-#   rake "run_aggregations4"
-#   rake "run_aggregations5"
-# end
-# 
-# every 1.day, :at => "3:40 pm" do
-#   rake "bls_nbis"
-# end
-# 
-# every 1.day, :at => "3:50 pm" do
-#   rake "gen_investigate_csv"
-# end
-
-
-
-
-
-
-
