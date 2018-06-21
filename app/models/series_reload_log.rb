7022bbcf6e52f538700edef9c242f864d2deb1c0
dji <David.Iannucci@hawaii.edu>
2018-06-21 12:53:33 -1000

    bugfix

--- app/models/series_reload_log.rb
+++ app/models/series_reload_log.rb
@@ -2,7 +2,7 @@ class SeriesReloadLog < ActiveRecord::Base
   self.primary_key = :batch_id, :series_id
 
   def SeriesReloadLog.purge_old_logs(horizon = 2.weeks)
-    old_logs = SeriesReloadLog.find_by_sql [<<~SQL, horizon]
+    old_logs = SeriesReloadLog.find_by_sql [<<~SQL, (Time.now - horizon)]
       select * from series_reload_logs where created_at < ?
     SQL
     old_logs.delete_all unless old_logs.empty?
