caa7ca991df1c73bba68eb538b20f2a5db9bef73
dji <David.Iannucci@hawaii.edu>
2018-07-31 15:44:33 -1000

    cleanup

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,16 +1,13 @@
 function replayCategoryOpenTree() {
     var nodesText = sessionStorage.getItem('udamanCatTreeOpenNodes');
-    console.log("!!!!!!!!!!!!!!!!!!!! THe nodeslist is "+nodesText);
     if (nodesText == undefined) return;
     var openNodeIds = Object.keys(JSON.parse(nodesText));
     for (var i = 0; i < openNodeIds.length; i++) {
-        console.log("^^^^^^^^^^^^^^^^^^ doing object "+openNodeIds[i]);
         categoryToggleUI(document.getElementById(openNodeIds[i]));
     }
 }
 
 function categoryToggleUI(node) {
-//    console.log("^^^^^^^^^^^^^^^^^^ object is "+the.toString());
     var ul = $(node).parent().children('ul');
     ul.toggle();
     var text = $(node).html();
@@ -27,21 +24,13 @@ function categoryToggleUI(node) {
 $(function() {
     $('.category_non_leaf').click(function() {
         var nodesText = sessionStorage.getItem('udamanCatTreeOpenNodes');
-        console.log("////////////////// Found stored opennodes |"+nodesText+"|");
         var openNodes = (nodesText == null) ? {} : JSON.parse(nodesText);
-//        var ul = $(this).parent().children('ul');
-//        ul.toggle();
-//        var text = $(this).html();
-//        if (ul.is(':hidden')) {
+        var thisId = $(this).attr('id');
         if (categoryToggleUI($(this))) {
-//            $(this).html(text.replace('-minus-', '-plus-'));
-            console.log("<<<<<<<<<<<< deleting from nodeslist: "+$(this).attr('id'));
-            delete openNodes[$(this).attr('id')];
+            delete openNodes[thisId];
         }
         else {
-//            $(this).html(text.replace('-plus-', '-minus-'));
-            console.log(">>>>>>>>>>> adding to nodeslist: "+$(this).attr('id'));
-            openNodes[$(this).attr('id')] = 1;
+            openNodes[thisId] = 1;
         }
         sessionStorage.setItem('udamanCatTreeOpenNodes', JSON.stringify(openNodes))
     });
