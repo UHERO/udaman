208deb725906a8b25aecd569b04e8dff969840f6
dji <David.Iannucci@hawaii.edu>
2018-08-07 12:28:49 -1000

    wip

--- app/helpers/series_helper.rb
+++ app/helpers/series_helper.rb
@@ -88,14 +88,14 @@ module SeriesHelper
     red = 255 if red > 255
     green = 20 if green < 20 
     blue = 20 if blue < 20
-    return "##{red.to_s(16)}#{green.to_s(16)}#{blue.to_s(16)}"
+    "##{red.to_s(16)}#{green.to_s(16)}#{blue.to_s(16)}"
   end
   
   def navigation_by_letter
     html = "<br />"
+    all_uhero = Series.get_all_uhero
     "A".upto("Z") do |letter|
-      count = Series.where("name LIKE :name", {:name => "#{letter}%"}).count
-      #count = Series.where(:name => /^#{letter}/)
+      count = all_uhero.where("name LIKE :name", { name: "#{letter}%" }).count
       if count > 0
         html += link_to raw("["+letter+"&nbsp;<span class='series_count'>#{count}</span>]"), {:action => 'index', :prefix => letter}
         html += " "
@@ -106,13 +106,14 @@ module SeriesHelper
   
   def navigation_by_frequency
     html = ""
+    all_uhero = Series.get_all_uhero
     [:month, :quarter, :year].each do |frequency|
-      count = Series.where(:frequency => frequency).count
+      count = all_uhero.where(frequency: frequency).count
       html += link_to raw(frequency.to_s + "&nbsp;<span class='series_count'>#{count}</span>"), {:action => 'index', :freq => frequency}
       html += "&nbsp;"
     end
-    count = Series.count
-	  html += link_to raw("all&nbsp;<span class='series_count'>#{count}</span>") , {:action => 'index', :all => 'true'}
+    count = all_uhero.count
+	  html + link_to raw("all&nbsp;<span class='series_count'>#{count}</span>") , {:action => 'index', :all => 'true'}
   end
 
   def nightly_actuator(nightly)
