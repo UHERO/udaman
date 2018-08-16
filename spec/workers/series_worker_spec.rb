da8a4b15b11dfd0dc7d0b110d8af6e880620cb2c
dji <David.Iannucci@hawaii.edu>
2018-08-15 14:42:54 -1000

    disabled spec testing for old SeriesWorker

--- spec/workers/series_worker_spec.rb
+++ spec/workers/series_worker_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 require 'redis'
 
+#
+# #
+# #
+# # THIS WHOLE MODULE IS FOR A VESTIGIAL BIT OF CODE, but I'll leave it here and disable
+# # for the time being rather than remove right away.
+# #
+# #
+#
+
 module RSpec
   module Sidekiq
     module Matchers
@@ -10,17 +19,17 @@ module RSpec
 end
 
 describe SeriesWorker do
-  redis = Redis.new
-  size = 35
-  redis.del "queue_#{size}"
-  it 'should grab from the queue' do
+  #redis = Redis.new
+  #size = 35
+  #redis.del "queue_#{size}"
+  xit 'should grab from the queue' do
     series_id = 1
     SeriesWorker.perform_async series_id, size
     expect(SeriesWorker).to have_enqueued_sidekiq_job(series_id, size)
     redis.del "queue_#{size}"
   end
 
-  it 'should decrease the namespaced queue' do
+  xit 'should decrease the namespaced queue' do
     series_id = Series.create(name: 'FAKE1@HI.A').id
     redis.set "series_list_#{size}", series_id
     redis.set "current_depth_#{size}", 8
