13b1bb6e90dddd927cfd4ba29fc4326e2dd2f64d
dji <David.Iannucci@hawaii.edu>
2018-09-26 17:11:48 -1000

    little more uncommenting... grasping....

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,8 @@
 #  # need to restart spork for it take effect.
 #
 #  # This file is copied to spec/ when you run 'rails generate rspec:install'
-#  ENV["RAILS_ENV"] ||= 'test'
-#  require File.expand_path("../../config/environment", __FILE__)
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
   # Requires supporting ruby files with custom matchers and macros, etc,
