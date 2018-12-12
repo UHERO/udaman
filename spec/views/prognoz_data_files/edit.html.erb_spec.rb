e3b73acefce982a15a4393db72efe2d10688f112
dji <David.Iannucci@hawaii.edu>
2018-12-12 12:05:09 -1000

    wip

--- spec/views/prognoz_data_files/edit.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe "prognoz_data_files/edit.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- spec/views/prognoz_data_files/write_xls.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe "prognoz_data_files/write_xls.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
