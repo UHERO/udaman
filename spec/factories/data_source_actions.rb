8f4aeb7084ffd2ea4919fb2215a5b728c3ca55e0
dji <David.Iannucci@hawaii.edu>
2018-12-31 14:27:21 -1000

    sillybug

--- spec/factories/data_source_actions.rb
+++ spec/factories/data_source_actions.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :data_source_action do
     data_source_id { 1 }
-    series_id 1 }
+    series_id { 1 }
     action { 'MyString' }
     eval { 'MyString' }
     priority { 100 }
