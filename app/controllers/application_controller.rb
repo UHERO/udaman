5179e075e9129f9a50370f2fe948c8c196bdd5e9
dji <David.Iannucci@hawaii.edu>
2019-01-03 16:10:43 -1000

    how did I miss fixing this right away?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_filter :authenticate_user!
+  before_action :authenticate_user!
   
   helper :all # include all helpers, all the time
   protect_from_forgery
