8bc7c98262a9c5718553a442da7ba412290ff0da
dji <David.Iannucci@hawaii.edu>
2019-01-03 17:00:31 -1000

    wip

--- lib/authorization.rb
+++ lib/authorization.rb
@@ -4,12 +4,6 @@ module Authorization
 
   EDIT_ACTIONS = %w(edit inline_update update up down new bulk_new create bulk_create)
 
-  included do
-    include ActiveSupport::Rescuable
-    # when we move to rails 5 we can use redirect_back instead of redirect_to(:back) below
-    rescue_from ActionController::RedirectBackError, with: :redirect_to_default
-  end
-
   def check_data_list_authorization
     if current_user.internal_user? && (%w(index new create create_copy).include?(params[:action]) || owns_data_list?(params[:id]))
       return
@@ -52,6 +46,10 @@ module Authorization
       redirect_to :back, flash: { error: 'Not authorized to destroy' }
       return
     end
+  end
+
+  def redirect_back_or_default(fallback = root_path)
+    redirect_back fallback_path: fallback, allow_other_host: false
   end
 
   def redirect_to_default
