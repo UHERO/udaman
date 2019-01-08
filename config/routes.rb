e88847ecc9e65152ef48e722289c7a53080bb9ca
dji <David.Iannucci@hawaii.edu>
2019-01-07 17:37:41 -1000

    put that back

--- config/routes.rb
+++ config/routes.rb
@@ -144,5 +144,5 @@ Rails.application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  ### obsolete, remove later ### match ':controller(/:action(/:id(.:format)))', via: :all
+  match ':controller(/:action(/:id(.:format)))', via: :all
 end
