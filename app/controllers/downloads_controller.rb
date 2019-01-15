96dd02ce68e2ae77307e0b94a765bb4dda7f4c7e
dji <David.Iannucci@hawaii.edu>
2019-01-14 17:23:54 -1000

    wip

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -31,9 +31,10 @@ class DownloadsController < ApplicationController
   end
   
   def update
-    post_params = download_params.delete(:post_parameters)
+    myparams = download_params
+    post_params = myparams.delete(:post_parameters)
     respond_to do |format|
-      if @output_file.update! download_params
+      if @output_file.update! myparams
         @output_file.process_post_params(post_params)
 
         format.html { redirect_to( :action => 'index', :notice => 'Download successfully updated.') }
