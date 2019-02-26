796dd8dd7c8dba3755d174ab966f9217ad22a0f8
dji <David.Iannucci@hawaii.edu>
2019-02-25 15:54:54 -1000

    wip

--- app/helpers/dbedt_uploads_helper.rb
+++ app/helpers/dbedt_uploads_helper.rb
@@ -2,28 +2,28 @@ module DbedtUploadsHelper
   def make_fa_processing_classes(status)
     case status
       when 'processing'
-        'fa-refresh fa-spin processing'
+        'fa-refresh fa-spinner processing'
       when 'ok'
         'fa-check ok'
       when 'fail'
         'fa-times fail'
       else
-        puts "unknown processing status: #{status}"
         'fa-times'
     end
   end
 
-  def make_fa_loading_classes(active)
-    if active == 'loading'
-      'fa-refresh fa-spin loading'
-    elsif active == 'yes'
-      'fa-dot-circle-o load-yes'
-    elsif active == 'no'
-      'fa-circle-o load-no'
-    elsif active == 'fail'
-      'fa-times load-fail'
-    end
-  end
+### this looks like vestigial code -- commenting out now, delete later
+#  def make_fa_loading_classes(active)
+#    if active == 'loading'
+#      'fa-refresh fa-spinner loading'
+#    elsif active == 'yes'
+#      'fa-dot-circle load-yes'
+#    elsif active == 'no'
+#      'fa-circle load-no'
+#    elsif active == 'fail'
+#      'fa-times load-fail'
+#    end
+#  end
 
   def now_processing?(cls)
      ## true if anything is currently in processing
