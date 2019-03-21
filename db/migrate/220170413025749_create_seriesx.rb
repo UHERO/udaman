3db245a1d0efac257a5f8c69e4e67e8f5d929cf3
dji <David.Iannucci@hawaii.edu>
2019-03-20 16:11:19 -1000

    wip

--- db/migrate/220170413025749_create_seriesx.rb
+++ db/migrate/220170413025749_create_seriesx.rb
@@ -3,8 +3,11 @@ class CreateSeriesx < ActiveRecord::Migration[5.2]
     execute <<~SQL
       CREATE TABLE seriesx LIKE series;
     SQL
+    #### WTH HAPPENS WITH PRIMARY KEY ID COLUMN IN NEW TABLE??
     add_column :data_points, :seriesx_id, :integer, null: false, default: 0, after: :series_id
     add_column :public_data_points, :seriesx_id, :integer, null: false, default: 0, after: :series_id
+    add_reference :seriesx, :series, foreign_key: true, after: :id
+    #### HOW TO RENAME ABOVE COLUMN AS main_series or whatever
     remove_column :data_points, :id
     remove_column :data_points, :universe
   end
