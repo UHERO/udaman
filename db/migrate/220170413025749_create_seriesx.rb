bded7e038f793f6d7416a43edb6a1589df2c24be
dji <David.Iannucci@hawaii.edu>
2019-03-21 12:29:28 -1000

    move the x from back to front

--- db/migrate/220170413025749_create_seriesx.rb
+++ db/migrate/220170413025749_create_seriesx.rb
@@ -1,28 +1,29 @@
 class CreateSeriesx < ActiveRecord::Migration[5.2]
   def self.up
     execute <<~SQL
-      CREATE TABLE seriesx LIKE series;
+      CREATE TABLE xseries LIKE series;
+      INSERT xseries SELECT * FROM series;
     SQL
-    add_reference :series, :seriesx, foreign_key: true, null: false, default: 0, after: :universe
-#   add_foreign_key :series, :seriesx, column: :seriesx_id
-    add_reference :data_points, :seriesx, foreign_key: true, null: false, default: 0, after: :series_id
-#   add_foreign_key :data_points, :seriesx, column: :seriesx_id
-    add_reference :public_data_points, :seriesx, foreign_key: true, null: false, default: 0, after: :series_id
-#   add_foreign_key :public_data_points, :seriesx, column: :seriesx_id
+    add_reference :series, :xseries, foreign_key: true, null: false, default: 0, after: :universe
+#   add_foreign_key :series, :xseries, column: :xseries_id
+    add_reference :data_points, :xseries, foreign_key: true, null: false, default: 0, after: :series_id
+#   add_foreign_key :data_points, :xseries, column: :xseries_id
+    add_reference :public_data_points, :xseries, foreign_key: true, null: false, default: 0, after: :series_id
+#   add_foreign_key :public_data_points, :xseries, column: :xseries_id
 
-    add_reference :seriesx, :series, foreign_key: true, null: false, default: 0, after: :id
-    rename_column :seriesx, :series_id, :primary_series_id
-#    add_column :seriesx, :id, :integer, first: true ### can we populate with auto increment?
-    remove_column :seriesx, :universe
-    remove_column :seriesx, :name
-    remove_column :seriesx, :description
-    remove_column :seriesx, :dataPortalName
-    remove_column :seriesx, :unit_id
-    remove_column :seriesx, :geography_id
-    remove_column :seriesx, :unitsLabelShort
-    remove_column :seriesx, :unitsLabel
-    remove_column :seriesx, :measurement_id
-    remove_column :seriesx, :scratch
+    add_reference :xseries, :series, foreign_key: true, null: false, default: 0, after: :id
+    rename_column :xseries, :series_id, :primary_series_id
+#    add_column :xseries, :id, :integer, first: true ### can we populate with auto increment?
+    remove_column :xseries, :universe
+    remove_column :xseries, :name
+    remove_column :xseries, :description
+    remove_column :xseries, :dataPortalName
+    remove_column :xseries, :unit_id
+    remove_column :xseries, :geography_id
+    remove_column :xseries, :unitsLabelShort
+    remove_column :xseries, :unitsLabel
+    remove_column :xseries, :measurement_id
+    remove_column :xseries, :scratch
 
     # cleanup
     remove_column :data_points, :id
@@ -31,9 +32,9 @@ class CreateSeriesx < ActiveRecord::Migration[5.2]
   end
 
   def self.down
-    drop_table :seriesx if table_exists? :seriesx
-    remove_column :series, :seriesx_id if column_exists? :series, :seriesx_id
-    remove_column :data_points, :seriesx_id if column_exists? :data_points, :seriesx_id
-    remove_column :public_data_points, :seriesx_id if column_exists? :public_data_points, :seriesx_id
+    drop_table :xseries if table_exists? :xseries
+    remove_column :series, :xseries_id if column_exists? :series, :xseries_id
+    remove_column :data_points, :xseries_id if column_exists? :data_points, :xseries_id
+    remove_column :public_data_points, :xseries_id if column_exists? :public_data_points, :xseries_id
   end
 end
