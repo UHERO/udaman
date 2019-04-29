3fda79938165e165b8b6c7466219161a8501ecf9
dji <David.Iannucci@hawaii.edu>
2019-04-29 11:27:04 -1000

    wip

--- app/lib/helper_utilities.rb
+++ app/lib/helper_utilities.rb
@@ -26,4 +26,10 @@ module HelperUtilities
     qnum = q.to_s.gsub(/\D/,'').to_i  ## allow for caller to pass things like "Q2"
     (qnum - 1) * 3 + 1
   end
+
+  def convert_qspec_to_date(str)
+    ## Quarter spec like YYYYQ1, YYYY-Q1, YYYY-Q01, "YYYY Q1", "YYYY Q01", etc
+    return nil unless str =~ /([12]\d\d\d)[-. ]*Q0?([1234])/i
+    '%s-%02d-01' % [$1, first_month_of_quarter($2)]
+  end
 end
--- app/lib/update_core.rb
+++ app/lib/update_core.rb
@@ -81,9 +81,9 @@ module UpdateCore
       return Date.parse cell_data.split(' ')[1] + semi_date
     end
 
-    if cell_data =~ /([12]\d\d\d)[-. ]?Q0?([1234])/i  ## Quarter spec like YYYYQ1, YYYY-Q1, YYYY-Q01, "YYYY Q1", etc
-      cell_data = '%s-%02d-01' % [$1, first_month_of_quarter($2)]
-    end
+    ## If it's a quarter spec (YYYYQ2, etc) then convert to a date (else don't)
+    cell_data = convert_qspec_to_date(cell_data) || cell_data
+
     Date.parse cell_data.to_s
   end
   
