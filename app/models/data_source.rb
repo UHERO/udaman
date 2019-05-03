6b5e4e9b6071a4c4a809f677189f9555db9bb926
dji <David.Iannucci@hawaii.edu>
2019-05-02 16:29:50 -1000

    pluralization geh

--- app/models/data_source.rb
+++ app/models/data_source.rb
@@ -4,7 +4,7 @@ class DataSource < ApplicationRecord
   require 'digest/md5'
   serialize :dependencies, Array
   
-  belongs_to :series, inverse_of: :data_source
+  belongs_to :series, inverse_of: :data_sources
   has_many :data_points, dependent: :delete_all
   has_many :data_source_downloads, dependent: :delete_all
   has_many :downloads, -> {distinct}, through: :data_source_downloads
