ab0fa84c24388acdce5d9d498b1011cbf38aa352
dji <David.Iannucci@hawaii.edu>
2019-09-06 12:34:59 -1000

    comment

--- app/helpers/data_points_helper.rb
+++ app/helpers/data_points_helper.rb
@@ -2,6 +2,6 @@ module DataPointsHelper
 
   def dp_superscript(dp)
     days_since_creation = (Time.now.to_date - dp.created_at.to_date).to_i
-    "#{days_since_creation}#{'(ph)' if dp.pseudo_history?}"
+    "#{days_since_creation}#{'(ph)' if dp.pseudo_history?}"  ## pseudo_history is a model attribute, not a code method
   end
 end
