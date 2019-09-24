835b1cafa78a46d01da7b7846a12bd3c6fe6833a
dji <David.Iannucci@hawaii.edu>
2019-09-23 16:49:39 -1000

    wip

--- db/migrate/220170413025753_change_default_decimals_again.rb
+++ db/migrate/220170413025753_change_default_decimals_again.rb
@@ -6,9 +6,12 @@ class ChangeDefaultDecimalsAgain < ActiveRecord::Migration[5.2]
     change_column_null :xseries, :decimals, false
     change_column_default :series, :decimals_ob, 2
     change_column_null :series, :decimals_ob, false
-    ## copy data between tables
-    rename_column :xseries, :decimals, :decimals_ob
-    rename_column :series, :decimals_ob, :decimals
+    execute <<~MYSQL
+      update series s join xseries x on x.id = s.xseries_id
+      set s.decimals_ob = x.decimals
+    MYSQL
+    rename_column :xseries, :decimals, :decimals_ob if column_exists? :xseries, :decimals
+    rename_column :series, :decimals_ob, :decimals if column_exists? :series, :decimals_ob
   end
   def self.down
     rename_column :xseries, :decimals_ob, :decimals if column_exists? :xseries, :decimals_ob
