5f97c890de927671a8e73fc1494002f38ab9d5f3
dji <David.Iannucci@hawaii.edu>
2019-09-24 11:15:53 -1000

    wip

--- db/migrate/220170413025753_change_default_decimals_again.rb
+++ db/migrate/220170413025753_change_default_decimals_again.rb
@@ -12,9 +12,64 @@ class ChangeDefaultDecimalsAgain < ActiveRecord::Migration[5.2]
     MYSQL
     rename_column :xseries, :decimals, :decimals_ob if column_exists? :xseries, :decimals
     rename_column :series, :decimals_ob, :decimals if column_exists? :series, :decimals_ob
+
+    ## Redefine series_v view
+    execute <<~MYSQL
+      create or replace view `series_v` as
+      select series.*,
+      x.frequency,
+      x.seasonally_adjusted,
+      x.seasonal_adjustment,
+      x.units,
+      x.percent,
+      x.real,
+      x.last_demetra_datestring,
+      x.last_demetra_date,
+      x.factors,
+      x.factor_application,
+      x.aremos_missing,
+      x.aremos_diff,
+      x.mult,
+      x.base_year,
+      x.frequency_transform,
+      x.restricted,
+      x.quarantined,
+      x.primary_series_id,
+      x.created_at as xs_created_at,
+      x.updated_at as xs_updated_at
+      from series join xseries x on x.id = series.xseries_id
+    MYSQL
   end
+
   def self.down
     rename_column :xseries, :decimals_ob, :decimals if column_exists? :xseries, :decimals_ob
     rename_column :series, :decimals, :decimals_ob if column_exists? :series, :decimals
+
+    execute <<~MYSQL
+      create or replace view `series_v` as
+      select series.*,
+      x.frequency,
+      x.seasonally_adjusted,
+      x.seasonal_adjustment,
+      x.units,
+      x.percent,
+      x.real,
+      x.decimals,
+      x.last_demetra_datestring,
+      x.last_demetra_date,
+      x.factors,
+      x.factor_application,
+      x.aremos_missing,
+      x.aremos_diff,
+      x.mult,
+      x.base_year,
+      x.frequency_transform,
+      x.restricted,
+      x.quarantined,
+      x.primary_series_id,
+      x.created_at as xs_created_at,
+      x.updated_at as xs_updated_at
+      from series join xseries x on x.id = series.xseries_id
+    MYSQL
   end
 end
