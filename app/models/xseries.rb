d19a0017b12079d73c2045dad25a79b0fc8c8147
dji <David.Iannucci@hawaii.edu>
2020-02-06 15:52:03 -1000

    wip

--- app/models/xseries.rb
+++ app/models/xseries.rb
@@ -11,9 +11,9 @@ class Xseries < ApplicationRecord
 private
   def last_rites
     primary_series.reload rescue return
-    Rails.logger.error { "ERROR: Cannot destroy xseries (id=#{id}) because its primary series still exists." }
-    #errors.add(:base, 'Cannot destroy xseries when its primary series still exists.')
-    throw(:abort)
+    message = "ERROR: Cannot destroy Xseries (id=#{id}) because its primary series still exists."
+    Rails.logger.error { message }
+    raise SeriesDestroyException, message
   end
 
 end
