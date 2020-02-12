8b30bf18d9d6cbab31acfd0b94ab3c261f27344b
dji <David.Iannucci@hawaii.edu>
2020-02-11 17:16:08 -1000

    wip

--- db/migrate/220170413025757_remove_last_ob_column.rb
+++ db/migrate/220170413025757_remove_last_ob_column.rb
@@ -1,5 +1,6 @@
 class RemoveLastObColumn < ActiveRecord::Migration[5.2]
   def change
     remove_column :public_data_points, :universe_ob if column_exists? :public_data_points, :universe_ob
+    add_index :series, [:universe, :name], unique: true
   end
 end
