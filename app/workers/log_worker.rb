eb8014258cf46a2a9824f152af9699a2788297f2
dji <David.Iannucci@hawaii.edu>
2020-02-12 19:26:47 -1000

    wip

--- app/workers/log_worker.rb
+++ app/workers/log_worker.rb
@@ -5,8 +5,8 @@ class LogWorker
   include Sidekiq::Worker
 
   def perform(level)
-    Rails.logger.level = level unless level.nil?  ## if nil, there's only logger output to report current state
-    Rails.logger.warn { "Logging level set to #{level}" }
+    Rails.logger.level = level unless level.nil?  ## if nil, there's only log output below to report current state
+    Rails.logger.warn { "Logging level now set to #{Rails.logger.level} (DEBUG=0 INFO=1 WARN=2 ERROR=3 FATAL=4)" }
   end
 
 end
