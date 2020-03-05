81c025a0300cc948985f640b21b86d61d8e34446
dji <David.Iannucci@hawaii.edu>
2020-03-04 18:09:44 -1000

    wip

--- script/gen_kau_dash.rb
+++ script/gen_kau_dash.rb
@@ -1,21 +1,37 @@
+require 'csv'
+
 Rails.logger.info { "Gen KAU dash: Start at #{Time.now}" }
 token = '-VI_yuv0UzZNy4av1SM5vQlkfPK_JKnpGfMzuJR7d0M='
-url = %q{https://api.uhero.hawaii.edu/v1/package/export?id=%d\&nocache}
 cmd = %q{curl --silent -H "Authorization: Bearer %s" } % token
+url = %q{https://api.uhero.hawaii.edu/v1/package/export?id=%d\&nocache}
 
-export_ids = %w{127 141 143}
+export_ids = %w{127 141 143}  ### probly automate this
+
 export_ids.each do |xid|
-  full_url = url % xid
-  response = %x{#{cmd + full_url}}
+  response = %x{#{cmd + url % xid}}
   json = JSON.parse response
+  data = {}
+  names = []
+  titles = {}
   series_array = json['data']
   series_array.each do |series|
     name = series['name']
-    title = series['title']
-    startdate = series['seriesObservations']['observationStart']
-    enddate = series['seriesObservations']['observationEnd']
+    names.push name
+    titles[name] = series['title']
     levels = series['seriesObservations']['transformationResults'][0]
-    data = levels['dates'].map {|date| [date, levels['values'].shift ] }.to_h
-    data
+    data[name] = levels['dates'].map {|date| [date, levels['values'].shift ] }.to_h
   end
+end
+CSV.generate do |csv|
+  csv << ['date'] + names
+  get_all_dates(data).each do |date|
+    csv << [date] + names.map {|series_name| data[series_name][date] }
+  end
+end
+Rails.logger.info { "Gen KAU dash: End at #{Time.now}" }
+
+def get_all_dates(series_data)
+  dates_array = []
+  series_data.each {|_, data| dates_array |= data.keys }
+  dates_array.sort
 end
