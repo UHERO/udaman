d2334de85d7df492e57be71f41654a4653cc4c6c
dji <David.Iannucci@hawaii.edu>
2020-03-04 16:56:28 -1000

    wip

--- script/gen_kau_dash.rb
+++ script/gen_kau_dash.rb
@@ -1,6 +1,6 @@
 Rails.logger.info { "Gen KAU dash: Start at #{Time.now}" }
 token = '-VI_yuv0UzZNy4av1SM5vQlkfPK_JKnpGfMzuJR7d0M='
-url = %q{https://api.uhero.hawaii.edu/v1/package/export?id=%d&nocache}
+url = %q{https://api.uhero.hawaii.edu/v1/package/export?id=%d\&nocache}
 cmd = %q{curl --silent -H "Authorization: Bearer %s" } % token
 
 export_ids = %w{127 141 143}
@@ -9,7 +9,13 @@ export_ids.each do |xid|
   response = %x{#{cmd + full_url}}
   json = JSON.parse response
   series_array = json['data']
-  series.array.each do |series|
-    ## do something
+  series_array.each do |series|
+    name = series['name']
+    title = series['title']
+    startdate = series['seriesObservations']['observationStart']
+    enddate = series['seriesObservations']['observationEnd']
+    levels = series['seriesObservations']['transformationResults'][0]
+    data_dates = levels['dates']
+    data_values = levels['values']
   end
 end
