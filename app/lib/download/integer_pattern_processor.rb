a7dceafe5f2aa78a41f8ed6c7f4f45a2924b3758
dji <David.Iannucci@hawaii.edu>
2020-03-12 13:26:45 -1000

    rename, rearrange

--- app/lib/download/integer_pattern_processor.rb
+++ app/lib/download/integer_pattern_processor.rb
@@ -34,10 +34,10 @@ class IntegerPatternProcessor
     p = @pattern.split(':')
     header_opts = { header_in: p[1], search_main: p[2].to_i, header_name: p[3], handle: handle, sheet: sheet }
     case p[0]
-      when 'increment' then pos_by_increment(p[1].to_i, p[2].to_i, index)
-      when 'repeat'    then pos_by_repeating_numbers(p[1].to_i, p[2].to_i, index)
-      when 'block'     then pos_by_repeating_number_x_times(p[1].to_i, p[2].to_i, p[3].to_i, index)
-      when 'repeat_with_step' then pos_by_repeating_numbers_with_step(p[1].to_i, p[2].to_i, p[3].to_i, index)
+      when 'increment' then increment(p[1].to_i, p[2].to_i, index)
+      when 'repeat'    then repeat_numbers(p[1].to_i, p[2].to_i, index)
+      when 'block'     then repeat_number_x_times(p[1].to_i, p[2].to_i, p[3].to_i, index)
+      when 'repeat_with_step' then repeat_numbers_with_step(p[1].to_i, p[2].to_i, p[3].to_i, index)
       when 'header' then DownloadPreprocessor.find_header(:header_name => p[3],
                                                            :header_in => p[1],
                                                            :search_main => p[2].to_i,
@@ -60,21 +60,21 @@ class IntegerPatternProcessor
 
 private
 
-  def pos_by_increment(start, step, index)
+  def increment(start, step, index)
     start + (step * index)
   end
 
-  def pos_by_repeating_numbers_with_step(first, last, step, index)
-    range = (last - first) / step + 1
-    (index % range) * step + first
-  end
-
-  def pos_by_repeating_numbers(first, last, index)
+  def repeat_numbers(first, last, index)
     range = last - first + 1
     (index % range) + first
   end
 
-  def pos_by_repeating_number_x_times(start, step, repeat, index)
+  def repeat_number_x_times(start, step, repeat, index)
     start + (index / repeat).truncate * step
+  end
+
+  def repeat_numbers_with_step(first, last, step, index)
+    range = (last - first) / step + 1
+    (index % range) * step + first
   end
 end
