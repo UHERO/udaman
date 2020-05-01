ec7223dbbbca705ca48e06253c0a60694b430a41
dji <David.Iannucci@hawaii.edu>
2020-04-30 18:44:07 -1000

    new migration

---
+++ db/migrate/220170413025760_add_disable_p_hto_data_source.rb
@@ -0,0 +1,8 @@
+class AddDisablePHtoDataSource < ActiveRecord::Migration[5.2]
+  def change
+    add_column :data_sources, :disabled, :boolean, null: false, default: false, after: :reload_nightly
+    add_column :data_sources, :pseudo_history, :boolean, null: false, default: false, after: :reload_nightly
+    change_column :data_sources, :priority, :integer, default: 100, after: :series_id  ## just move it to the left
+    change_column :data_sources, :runtime, :float, after: :presave_hook  ## just move it to the right
+  end
+end
