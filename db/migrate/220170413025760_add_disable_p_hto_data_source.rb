8241e5690a0c41a08101658c43beef2c95f3530c
dji <David.Iannucci@hawaii.edu>
2020-05-01 16:02:41 -1000

    wip

--- db/migrate/220170413025760_add_disable_p_hto_data_source.rb
+++ db/migrate/220170413025760_add_disable_p_hto_data_source.rb
@@ -1,7 +1,7 @@
 class AddDisablePHtoDataSource < ActiveRecord::Migration[5.2]
   def change
-    add_column :data_sources, :disabled, :boolean, null: false, default: false, after: :reload_nightly
-    add_column :data_sources, :pseudo_history, :boolean, null: false, default: false, after: :reload_nightly
+    add_column :data_sources, :disabled, :boolean, null: false, default: false, after: :reload_nightly unless column_exists? :data_sources, :disabled
+    add_column :data_sources, :pseudo_history, :boolean, null: false, default: false, after: :reload_nightly unless column_exists? :data_sources, :pseudo_history
     change_column :data_sources, :priority, :integer, default: 100, after: :series_id  ## just move it to the left
     change_column :data_sources, :runtime, :float, after: :presave_hook  ## just move it to the right
   end
