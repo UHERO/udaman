7f4d36bda3fd2ddfb8835c5a383fa0e0e19571b5
dji <David.Iannucci@hawaii.edu>
2020-05-20 11:12:13 -1000

    change to up/down format

--- db/migrate/220170413025761_add_index_to_fore_snaps.rb
+++ db/migrate/220170413025761_add_index_to_fore_snaps.rb
@@ -1,6 +1,10 @@
 class AddIndexToForeSnaps < ActiveRecord::Migration[5.2]
-  def change
+  def self.up
     add_index :forecast_snapshots, [:name, :version], unique: true unless index_exists? :forecast_snapshots, [:name, :version]
-    change_column :forecast_snapshots, :comments, :text, after: :history_tsd_label    ### just move the position for convenience
+    change_column :forecast_snapshots, :comments, :text, after: :history_tsd_label    ### just move position for convenience
+  end
+
+  def self.down
+    remove_index :forecast_snapshots, [:name, :version], unique: true if index_exists? :forecast_snapshots, [:name, :version]
   end
 end
