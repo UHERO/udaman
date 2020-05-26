992376b073a8fbecad1c287a51416281130e0c97
dji <David.Iannucci@hawaii.edu>
2020-05-25 16:10:49 -1000

    wip

--- app/helpers/forecast_snapshots_helper.rb
+++ app/helpers/forecast_snapshots_helper.rb
@@ -1,8 +1,10 @@
 module ForecastSnapshotsHelper
 
   def generate_date_range_controls(table_start, table_end)
-    ' From ' + select_tag('table_start', options_for_select(@all_dates.each_with_index.to_a, table_start)) +
-      ' To ' + select_tag('table_end',   options_for_select(@all_dates.each_with_index.to_a, table_end))
+    is_quar = @all_dates.any? {|s| s =~ /-(04|07|10)-/ }
+    menu_list = @all_dates.each_with_index.to_a.map {|d, i| [is_quar ? date_to_qspec(d) : d[0..3], i] }
+    select_tag('table_start', options_for_select(menu_list, table_start)) + ' from-to ' +
+    select_tag('table_end',   options_for_select(menu_list, table_end))
   end
 
 end
