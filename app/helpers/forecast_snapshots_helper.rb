8014147c3e1dcda301096282ae9e3151d992dccd
dji <David.Iannucci@hawaii.edu>
2020-05-27 13:17:15 -1000

    wip

--- app/helpers/forecast_snapshots_helper.rb
+++ app/helpers/forecast_snapshots_helper.rb
@@ -8,4 +8,21 @@ module ForecastSnapshotsHelper
     select_tag('table_end',   options_for_select(end_menu_list, table_end))
   end
 
+  def forecast_snapshot_csv_gen
+    CSV.generate do |csv|
+=begin
+      series_data = @export.series_data
+      names = Export.connection.execute(%Q|SELECT series.name AS name
+          FROM export_series
+          LEFT JOIN series ON series.id = export_series.series_id
+          WHERE export_series.export_id = #{@export.id}
+          ORDER BY export_series.list_order;|).to_a.flatten
+=end
+      dates_array = @export.data_dates
+      csv << ['date'] + names
+      dates_array.each do |date|
+        csv << [date] + names.map {|series_name| series_data[series_name][date]}
+      end
+    end
+  end
 end
