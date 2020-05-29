25c5dfa8cfa0ba4dec10e6f190e32fba0294e8c4
kotodharma <kotodharma@users.noreply.github.com>
2020-05-28 15:50:57 -1000

    Followup improvements to Forecast Snaps (UA-1313)

--- app/helpers/forecast_snapshots_helper.rb
+++ app/helpers/forecast_snapshots_helper.rb
@@ -8,4 +8,27 @@ module ForecastSnapshotsHelper
     select_tag('table_end',   options_for_select(end_menu_list, table_end))
   end
 
+  def forecast_snapshot_csv_gen
+    CSV.generate do |csv|                                                                    ## save memory by 86ing unneeded bits
+      newstuff = @tsd_files[0].get_all_series.map {|hash| hash.tap {|h| h[:name] += ' (new)'; h[:data] = h[:yoy_hash] = nil } }
+      oldstuff = @tsd_files[1].get_all_series.map {|hash| hash.tap {|h| h[:name] += ' (old)'; h[:data] = h[:yoy_hash] = nil } }
+      hisstuff = @tsd_files[2].get_all_series.map {|hash| hash.tap {|h| h[:name] += ' (his)'; h[:data] = h[:yoy_hash] = nil } }
+      all = (newstuff + oldstuff + hisstuff).map {|h| [h[:name], h] }.to_h
+      names = all.keys.sort do |a, b|
+        (a0, a1) = a.split
+        (b0, b1) = b.split
+        fc = { '(new)' => 0, '(old)' => 1, '(his)' => 2 }
+        (a0 <=> b0) == 0 ? fc[a1] <=> fc[b1] : a0 <=> b0
+      end
+      dates = []
+      all.each do |_, v|
+        dates |= v[:data_hash].keys
+      end
+      csv << ['Date'] + names
+      dates.sort.each do |date|
+        csv << [date] + names.map {|name| '%0.3f' % all[name][:data_hash][date] rescue nil }
+      end
+    end
+  end
+
 end
