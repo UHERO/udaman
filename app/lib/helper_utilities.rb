a6be6a5551c77146809c3a63ccf16c96a66be670
dji <David.Iannucci@hawaii.edu>
2020-06-08 17:33:33 -1000

    new comments in helper

--- app/lib/helper_utilities.rb
+++ app/lib/helper_utilities.rb
@@ -7,16 +7,20 @@ module HelperUtilities
   end
 
   ## Put in month number, get out quarter number
+  ## e.g. quarter_by_month(11) => 4
   def quarter_by_month(mon)
     (mon.to_i - 1) / 3 + 1
   end
 
+  ## Put in quarter spec like YYYYQ1, YYYY-Q1, YYYY/Q1, YYYY-Q01, "YYYY Q1", "YYYY Q01", etc
+  ## get out date form, e.g. qspec_to_date("2012Q2") => "2012-04-01"
   def qspec_to_date(qstr)
-    ## Quarter spec like YYYYQ1, YYYY-Q1, YYYY/Q1, YYYY-Q01, "YYYY Q1", "YYYY Q01", etc
     return nil unless qstr =~ /([12]\d\d\d)[-.\/ ]*Q0?([1234])/i
     '%s-%02d-01' % [$1, first_month_of_quarter($2)]
   end
 
+  ## Put in date, get out quarter spec
+  ## e.g. date_to_qspec("2018-03-01") => "2018Q1"
   def date_to_qspec(date)
     unless date.class === Date
       date = Date.parse(date) rescue raise("date_to_qspec: parameter #{date} not a proper date string")
