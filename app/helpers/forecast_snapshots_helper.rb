b39c91a6316a643a17089f12f180c6cb2fb06836
dji <David.Iannucci@hawaii.edu>
2020-06-11 14:15:11 -1000

    wip

--- app/helpers/forecast_snapshots_helper.rb
+++ app/helpers/forecast_snapshots_helper.rb
@@ -1,11 +1,11 @@
 module ForecastSnapshotsHelper
 
-  def generate_date_range_controls(table_start, table_end)
-    date_f = @all_dates.any? {|s| s =~ /-(04|07|10)-/ } ? lambda {|d| date_to_qspec(d) } : lambda {|d| d[0..3] }
-    start_menu_list = @all_dates.each_with_index.to_a.map {|d, i| [date_f.call(d), i, { id: "fstab-start-#{i}" }] }
-    end_menu_list   = @all_dates.each_with_index.to_a.map {|d, i| [date_f.call(d), i, { id: "fstab-end-#{i}" }] }
-    select_tag('table_start', options_for_select(start_menu_list, table_start)) + ' from-to ' +
-    select_tag('table_end',   options_for_select(end_menu_list, table_end))
+  def generate_date_range_controls(sample_from, sample_to)
+    date_f = @is_quarterly ? lambda {|d| date_to_qspec(d) } : lambda {|d| d[0..3] }
+    from_menu_list = @all_dates.each_with_index.to_a.map {|d, i| [date_f.call(d), i, { id: "fstab-from-#{i}" }] }
+    to_menu_list   = @all_dates.each_with_index.to_a.map {|d, i| [date_f.call(d), i, { id: "fstab-to-#{i}" }] }
+    select_tag('sample_from', options_for_select(from_menu_list, sample_from)) + ' from-to ' +
+    select_tag('sample_to',   options_for_select(to_menu_list, sample_to))
   end
 
   def data_portal_link(series)
