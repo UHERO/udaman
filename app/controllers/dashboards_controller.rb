7da5b5bf3773c4f3e44018c14cbe82ea84393ae1
dji <David.Iannucci@hawaii.edu>
2020-06-18 17:59:00 -1000

    wip

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,27 +3,6 @@ class DashboardsController < ApplicationController
 
   before_action :check_authorization
 
-  def index
-    @series_count = Series.count
-    @aremos_series_count = AremosSeries.count
-    @not_in_db = (AremosSeries.all_names - Series.all_names).count
-    @in_db = @aremos_series_count - @not_in_db
-    @frequency_counts = Series.frequency_counts
-    @region_counts = Series.region_counts
-    
-    @a_counts = Series.last_observation_buckets('year')
-    @s_counts = Series.last_observation_buckets('semi')
-    @q_counts = Series.last_observation_buckets('quarter')
-    @m_counts = @q_counts
-    #@m_counts = Series.last_observation_buckets('month')
-    
-    @data_source_count = DataSource.count
-    @type_buckets = DataSource.type_buckets
-    #don't want to show the loads, only the transformations
-    @sa_count = @type_buckets.delete :sa_load
-    @load_count = @type_buckets.delete(:load) + @sa_count + @type_buckets[:mean_corrected_load]
-  end
-
   def investigate
     @maybe_ok = Series.get_all_uhero.where('aremos_missing = 0 AND ABS(aremos_diff) < 1.0 AND ABS(aremos_diff) > 0.0').order('aremos_diff DESC')
     @wrong = Series.get_all_uhero.where('aremos_missing = 0 AND ABS(aremos_diff) >= 1.0').order('aremos_diff DESC')
