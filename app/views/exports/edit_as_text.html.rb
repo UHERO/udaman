542ca0957793f93e1d0016b1c3cc7a4330f03b94
dji <David.Iannucci@hawaii.edu>
2020-08-06 20:46:20 -1000

    wip

--- app/views/exports/edit_as_text.html.rb
+++
@@ -1,9 +0,0 @@
-<h1>Editing Series for <%= @export.name %></h1>
-
-<%= form_with url: { action: :save_as_text, id: @data_list } do |f| %>
-  <%= text_area_tag :edit_box, @prefix_list, rows: 30, cols: 30 %>
-  <p>
-  <%= f.submit 'Store list' %>
-  <%= link_to 'Cancel', :back %>
-  </p>
-<% end %>
