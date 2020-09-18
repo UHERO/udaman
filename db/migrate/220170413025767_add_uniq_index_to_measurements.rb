ee9f524e6ddb48a87c3cdb4fa4addc72d95c1fad
dji <David.Iannucci@hawaii.edu>
2020-09-17 21:22:54 -1000

    wip

--- db/migrate/220170413025767_add_uniq_index_to_measurements.rb
+++ db/migrate/220170413025767_add_uniq_index_to_measurements.rb
@@ -1,9 +1,11 @@
 class AddUniqIndexToMeasurements < ActiveRecord::Migration[5.2]
   def self.up
-    add_index :measurements, [:universe, :prefix], unique: true unless index_exists? :measurements, [:universe, :prefix]
+    #add_index :measurements, [:universe, :prefix], unique: true unless index_exists? :measurements, [:universe, :prefix]
+    true
   end
 
   def self.down
-    remove_index :measurements, [:universe, :prefix], unique: true if index_exists? :measurements, [:universe, :prefix]
+    #remove_index :measurements, [:universe, :prefix], unique: true if index_exists? :measurements, [:universe, :prefix]
+    true
   end
 end
