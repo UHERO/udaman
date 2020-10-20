cd2834e7f2140ef96eb629bd5b87d1a62312e49f
dji <David.Iannucci@hawaii.edu>
2020-10-19 23:55:28 -1000

    wip

--- app/helpers/data_points_helper.rb
+++ app/helpers/data_points_helper.rb
@@ -4,4 +4,15 @@ module DataPointsHelper
     days_since_creation = (Time.now.to_date - dp.created_at.to_date).to_i
     "#{days_since_creation}#{'(ph)' if dp.pseudo_history?}"  ## pseudo_history is a model attribute, not a code method
   end
+
+  def dp_age_code(dp)
+    days_old = (Time.now.to_date - dp.created_at.to_date).to_i
+    case
+    when days_old < 99
+      days_old.to_s
+    when (days_old / 30.0).round < 10
+      (days_old / 30.0).round.to_s + 'M'
+    else 'XY'
+    end
+  end
 end
