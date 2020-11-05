eefe4241559fa64964772dd2db261cd0f797d770
dji <David.Iannucci@hawaii.edu>
2020-11-04 22:33:32 -1000

    wip

--- app/helpers/data_points_helper.rb
+++ app/helpers/data_points_helper.rb
@@ -1,10 +1,5 @@
 module DataPointsHelper
 
-  def dp_superscript(dp)
-    days_since_creation = (Time.now.to_date - dp.created_at.to_date).to_i
-    "#{days_since_creation}#{'(ph)' if dp.pseudo_history?}"  ## pseudo_history is a model attribute, not a code method
-  end
-
   def dp_display(value, bgcolor, age: nil, title: nil)
     tane = '%.3f' % value
     tane += ' (%s)' % age if age
@@ -15,14 +10,15 @@ module DataPointsHelper
   def dp_age_code(dp)
     days = (Time.now.to_date - dp.created_at.to_date).to_i
     months = (days / 30.0).round
-    case
-      when  days < 100 then '%02d' % days
-      when months < 10 then "#{months}M"
-      else
-        years = days / 365.0
-        years = (years % 1) < 0.80 ? years.to_i : years.to_i + 1
-        years = 1 if years == 0
-        "#{years}Y"
-    end
+    age = case
+          when  days < 100 then '%02d' % days
+          when months < 10 then "#{months}M"
+          else
+            years = days / 365.0
+            years = (years % 1) < 0.80 ? years.to_i : years.to_i + 1
+            years = 1 if years == 0
+            "#{years}Y"
+          end
+    age + (dp.pseudo_history? ? '(ph)' : '')
   end
 end
