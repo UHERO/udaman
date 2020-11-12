aba9661aba09367aadd54979fcceecfd23fd0231
kotodharma <kotodharma@users.noreply.github.com>
2020-11-12 13:49:49 -1000

    Restart REST API button in udaman (UA-1382)

--- config/routes.rb
+++ config/routes.rb
@@ -81,6 +81,7 @@ Rails.application.routes.draw do
   get 'investigate' => 'dashboards#investigate'
   get 'investigate_visual' => 'dashboards#investigate_visual'
   post 'update_public_dp' => 'dashboards#update_public_dp'
+  post 'restart_restapi' => 'dashboards#restart_restapi'
   get 'export_tsd' => 'dashboards#export_tsd'
   get 'autocomplete' => 'series#autocomplete_search'
   get 'series/search' => 'series#new_search'
