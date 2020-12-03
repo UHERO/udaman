031cc873ce51deebbde69161e9b2fc299a292464
dji <David.Iannucci@hawaii.edu>
2020-12-03 00:41:59 -1000

    not sure this will work, but we give it a try

--- app/models/series.rb
+++ app/models/series.rb
@@ -730,7 +730,7 @@ class Series < ApplicationRecord
     descript = "loaded from #{handle} into a series of files"
     if Series.valid_download_handle(handle, date_sensitive: false)
       path = Download.get(handle, :nondate).save_path rescue raise("Unknown download handle #{handle}")
-      descript = "loaded from download to <#{path}>"
+      descript = "loaded from download to <#{data_path_relativize(path)}>"
     end
     Series.new_transformation(descript, series_data, frequency_from_code(options[:frequency]))
   end
