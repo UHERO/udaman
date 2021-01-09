8acc57df5a6abddb1cebe2254f31b865ec179e3a
dji <David.Iannucci@hawaii.edu>
2021-01-08 20:49:57 -1000

    wip

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -31,7 +31,6 @@ class UnitsController < ApplicationController
     unit_params[:long_label].strip!
     @unit = Unit.new(unit_params)
     error = 'Unknown error'
-    puts "----------->>>>>>>>>>>>>>>>>>>> par #{unit_params}, obj #{@unit} <<<< "
 
     begin
       saved = @unit.save
