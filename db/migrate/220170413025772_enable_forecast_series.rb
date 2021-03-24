055b182d2caf540af9928b8d3cabfd6bbc6de8c7
dji <David.Iannucci@hawaii.edu>
2021-03-24 12:42:04 -1000

    wip

---
+++ db/migrate/220170413025772_enable_forecast_series.rb
@@ -0,0 +1,10 @@
+class EnableForecastSeries < ActiveRecord::Migration[5.2]
+  def self.up
+    change_column :series, :xseries_id, :integer, after: :id
+    add_column :series, :forecast, :boolean, null: false, default: false, after: :universe unless column_exists? :series, :forecast
+  end
+
+  def self.down
+    remove_column :series, :forecast, :boolean if column_exists? :series, :forecast
+  end
+end
