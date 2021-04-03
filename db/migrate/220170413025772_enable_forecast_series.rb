fe7a25b0d9b6cdb72905c80221d3379e5d6c2b61
dji <David.Iannucci@hawaii.edu>
2021-04-02 15:58:45 -1000

    wow dude dumb

--- db/migrate/220170413025772_enable_forecast_series.rb
+++ db/migrate/220170413025772_enable_forecast_series.rb
@@ -2,9 +2,9 @@ class EnableForecastSeries < ActiveRecord::Migration[5.2]
   def self.up
     change_column :series, :xseries_id, :integer, after: :id
     change_column :series, :geography_id, :integer, after: :id
-    change_column :series, :universe, %q(ENUM('UHERO','FC',DBEDT','NTA','COH','CCOM')), null: false, default: 'UHERO'
-    change_column :geographies, :universe, %q(ENUM('UHERO','FC',DBEDT','NTA','COH','CCOM')), null: false, default: 'UHERO'
-    change_column :data_sources, :universe, %q(ENUM('UHERO','FC',DBEDT','NTA','COH','CCOM')), null: false, default: 'UHERO'
+    change_column :series, :universe, %q(ENUM('UHERO','FC','DBEDT','NTA','COH','CCOM')), null: false, default: 'UHERO'
+    change_column :geographies, :universe, %q(ENUM('UHERO','FC','DBEDT','NTA','COH','CCOM')), null: false, default: 'UHERO'
+    change_column :data_sources, :universe, %q(ENUM('UHERO','FC','DBEDT','NTA','COH','CCOM')), null: false, default: 'UHERO'
   end
 
   def self.down
