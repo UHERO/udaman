707aa1501091a106aa0a4c7b3c7a16467030678d
dji <David.Iannucci@hawaii.edu>
2021-04-05 18:39:44 -1000

    wip

--- app/lib/series_arithmetic.rb
+++ app/lib/series_arithmetic.rb
@@ -112,15 +112,17 @@ module SeriesArithmetic
 
   ## Generalized computation of change in level. Can be used for YOY, etc by changing the offset.
   def level_change(offset: nil)
+    include ActionView::Helpers::DateHelper
     new_series = {}
     last_val = nil
     data.sort.each do |date, value|
       next if value.nil?
-      prev = (offset.nil? ? last_val : data[date - offset]) || next
+      prev = (offset ? data[date - offset] : last_val) || next
       new_series[date] = value - prev
       last_val = value
     end
-    new_transformation("Level change of #{self} #{}", new_series)
+    offset_s = offset && " w/offset #{distance_of_time_in_words(offset).sub('about ','')}"
+    new_transformation("Level change of #{self}#{offset_s}", new_series)
   end
 
   def absolute_change(id = nil)
