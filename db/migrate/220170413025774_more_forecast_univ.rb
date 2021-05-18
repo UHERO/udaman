06392549b1d93ecce8af503e782a5d6652bba4b1
dji <David.Iannucci@hawaii.edu>
2021-05-17 15:44:47 -1000

    wip

--- db/migrate/220170413025774_more_forecast_univ.rb
+++ db/migrate/220170413025774_more_forecast_univ.rb
@@ -1,11 +1,17 @@
 class MoreForecastUniv < ActiveRecord::Migration[5.2]
   def self.up
     change_column :categories, :universe, %q(ENUM('UHERO','FC','DBEDT','NTA','COH','CCOM')), null: false, default: 'UHERO'
+    change_column :data_lists, :universe, %q(ENUM('UHERO','FC','DBEDT','NTA','COH','CCOM')), null: false, default: 'UHERO'
+    change_column :feature_toggles, :universe, %q(ENUM('UHERO','FC','DBEDT','NTA','COH','CCOM')), null: false, default: 'UHERO'
     change_column :measurements, :universe, %q(ENUM('UHERO','FC','DBEDT','NTA','COH','CCOM')), null: false, default: 'UHERO'
+    change_column :units, :universe, %q(ENUM('UHERO','FC','DBEDT','NTA','COH','CCOM')), null: false, default: 'UHERO'
   end
 
   def self.down
     change_column :categories, :universe, %q(ENUM('UHERO','DBEDT','NTA','COH','CCOM')), null: false, default: 'UHERO'
+    change_column :data_lists, :universe, %q(ENUM('UHERO','DBEDT','NTA','COH','CCOM')), null: false, default: 'UHERO'
+    change_column :feature_toggles, :universe, %q(ENUM('UHERO','DBEDT','NTA','COH','CCOM')), null: false, default: 'UHERO'
     change_column :measurements, :universe, %q(ENUM('UHERO','DBEDT','NTA','COH','CCOM')), null: false, default: 'UHERO'
+    change_column :units, :universe, %q(ENUM('UHERO','DBEDT','NTA','COH','CCOM')), null: false, default: 'UHERO'
   end
 end
