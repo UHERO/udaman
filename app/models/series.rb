5316288e0a7cf7ae6aaf3b75d4f3195ced83ec5c
dji <David.Iannucci@hawaii.edu>
2021-07-30 14:54:35 -1000

    wip

--- app/models/series.rb
+++ app/models/series.rb
@@ -17,6 +17,7 @@ class Series < ApplicationRecord
 
   validates :name, presence: true, uniqueness: { scope: :universe }
   validate :source_link_is_valid
+  validate :descriptive_text_is_valid
   before_destroy :last_rites, prepend: true
   after_destroy :post_mortem, prepend: true
 
@@ -1122,16 +1123,6 @@ class Series < ApplicationRecord
     eval_statements.each {|es| eval(es)}
   end
 
-  ## this appears to be vestigial. Renaming now; if nothing breaks, delete later
-  def delete_with_data_DELETEME?
-    puts "deleting #{name}"
-    data_sources.each do |ds|
-      puts "deleting: #{ds.id}" + ds.eval 
-      ds.delete
-    end
-    self.delete
-  end
-
   def Series.get_all_series_by_eval(patterns)
     if patterns.class == String
       patterns = [patterns]
@@ -1190,14 +1181,6 @@ class Series < ApplicationRecord
     series_success
   end
 
-  ## this appears to be vestigial. Renaming now; if nothing breaks, delete later
-  def Series.missing_from_aremos_DELETEME?
-    name_buckets = {}
-    (AremosSeries.all_names - Series.all_names).each {|name| name_buckets[name[0]] ||= []; name_buckets[name[0]].push(name)}
-    name_buckets.each {|letter, names| puts "#{letter}: #{names.count}"}
-    name_buckets
-  end
-  
   def Series.search_box(input_string, limit: 10000, user_id: nil)
     all = Series.joins(:xseries)
     univ = 'UHERO'
@@ -1388,18 +1371,6 @@ class Series < ApplicationRecord
     Rails.logger.info { "Assign_dependency_depth: done at #{Time.now}" }
   end
 
-  ## probably vestigial - make sure, then delete later
-  def increment_dependency_depth_DELETEME
-    self.dependency_depth += 1
-    dependencies = []
-    self.enabled_data_sources.each do |ds|
-      dependencies += ds.dependencies
-    end
-    dependencies.uniq.each do |dependency|
-      Series.get(dependency).increment_dependency_depth
-    end
-  end
-
   def get_all_dependencies
     Series.get_all_dependencies([self.id])
   end
@@ -1442,6 +1413,10 @@ class Series < ApplicationRecord
 
   def source_link_is_valid
     source_link.blank? || Series.valid_url(source_link) || errors.add(:source_link, 'is not a valid URL')
+  end
+
+  def descriptive_text_is_valid
+    dataPortalName.blank? && description.blank? && errors.add('Cannot save a Series without Data Portal Name and/or Description')
   end
 
   def force_destroy!
