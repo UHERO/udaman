c97d671d1a18b8460ecff8dd37a5c69fd270bb80
dji <David.Iannucci@hawaii.edu>
2021-08-14 18:38:15 -1000

    &nodpm in search

--- app/models/series.rb
+++ app/models/series.rb
@@ -1251,6 +1251,7 @@ class Series < ApplicationRecord
                           when 'r'   then %q{restricted = true}
                           when 'sa'  then %q{seasonal_adjustment = 'seasonally_adjusted'}
                           when 'ns'  then %q{seasonal_adjustment = 'not_seasonally_adjusted'}
+                          when 'nodpm'  then %Q{dataPortalName is #{negated}null}
                           when 'nodata' then %q{(not exists(select * from data_points where xseries_id = xseries.id and current))}
                           when 'noclip'
                             raise 'No user identified for clipboard access' if user_id.nil?
