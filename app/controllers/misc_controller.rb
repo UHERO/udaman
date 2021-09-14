abbf4d37c3d80602f2f5792b7fb6262a56712ddc
dji <David.Iannucci@hawaii.edu>
2021-09-14 12:56:03 -1000

    wip

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -5,10 +5,10 @@ class MiscController < ApplicationController
 
   def get_branch_code
     ApplicationRecord.connection.execute(<<~MYSQL) rescue raise('Update last_branch_code_number failed')
-    update branch_code set last_branch_code_number = last_branch_code_number + 1
+      update branch_code set last_branch_code_number = last_branch_code_number + 1
     MYSQL
     results = ApplicationRecord.connection.execute(<<~MYSQL) rescue raise('Select last_branch_code_number failed')
-    select last_branch_code_number from branch_code limit 1
+      select last_branch_code_number from branch_code limit 1
     MYSQL
     @code = results.to_a.pop.pop.to_i
   end
