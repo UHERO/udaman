35e5faee12e5da382b596ac152d48f520ea99cf6
dji <David.Iannucci@hawaii.edu>
2021-09-14 12:47:10 -1000

    wip

--- db/migrate/220170413025777_create_misc_stuff.rb
+++ db/migrate/220170413025777_create_misc_stuff.rb
@@ -1,8 +1,8 @@
 class CreateMiscStuff < ActiveRecord::Migration[5.2]
   def change
-    drop table :data_load_patterns
-    drop table :data_portal_names
-    drop table :tsd_files
+    drop_table :data_load_patterns
+    drop_table :data_portal_names
+    drop_table :tsd_files
 
     create_table :branch_code do |t|
       t.integer :last_branch_code_number, null: false, default: 0
