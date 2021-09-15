45953c91fe019a2a415fcf734d54c59be335dd1b
dji <David.Iannucci@hawaii.edu>
2021-09-15 10:02:18 -1000

    wip

--- db/migrate/220170413025778_oops_tsd_restore.rb
+++ db/migrate/220170413025778_oops_tsd_restore.rb
@@ -1,5 +1,5 @@
 class OopsTsdRestore < ActiveRecord::Migration[5.2]
-  def change
+  def self.up
     create_table "tsd_files", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
       t.integer "forecast_snapshot_id"
       t.string "filename"
@@ -7,5 +7,9 @@ class OopsTsdRestore < ActiveRecord::Migration[5.2]
       t.datetime "created_at", null: false
       t.datetime "updated_at", null: false
     end
+  end
+
+  def self.down
+    drop_table :tsd_files
   end
 end
