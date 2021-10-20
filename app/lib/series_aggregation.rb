104b99402e018ad548abd73647526531698b9537
dji <David.Iannucci@hawaii.edu>
2021-10-19 18:09:07 -1000

    wip

--- app/lib/series_aggregation.rb
+++ app/lib/series_aggregation.rb
@@ -8,9 +8,9 @@ module SeriesAggregation
     end
     grouped_data = orig_series.group_data_by(frequency, prune: prune)
     aggregated_data = {}
-    grouped_data.keys.each do |date_string|
+    grouped_data.keys.each do |date|
       begin
-        aggregated_data[date_string] = grouped_data[date_string].send(operation)
+        aggregated_data[date] = grouped_data[date].send(operation)
       rescue NoMethodError
         raise "Method #{operation} is not implemented"
       end
