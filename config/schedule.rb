06d843f2face2500db19649e115194662ee15279
kotodharma <kotodharma@users.noreply.github.com>
2023-02-28 00:45:26 -1000

    Update EIA API to v2, plus misc other

--- config/schedule.rb
+++ config/schedule.rb
@@ -34,10 +34,6 @@ every 1.day, :at => '6:09 pm' do
   rake :reset_dependency_depth
 end
 
-every 1.day, :at => '8:00 pm' do
-  rake :reload_aremos
-end
-
 every 1.day, :at => '1:00 am' do
   rake :update_seats_links
   rake :update_vis_history_links
@@ -81,10 +77,6 @@ end
 
 every :day, at: '6:30 am' do
   rake :gen_investigate_csv
-end
-
-every :day, at: '6:50 am' do
-  rake :aremos_exports
 end
 
 every :day, at: '8:50 am' do
