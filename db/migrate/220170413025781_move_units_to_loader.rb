f5e42d1e1fb2aef760dbaf0f1d7c675722f9cce4
dji <David.Iannucci@hawaii.edu>
2023-03-30 18:44:51 -1000

    wip

--- db/migrate/220170413025781_move_units_to_loader.rb
+++ db/migrate/220170413025781_move_units_to_loader.rb
@@ -1,5 +1,5 @@
 class MoveUnitsToLoader < ActiveRecord::Migration[5.2]
   def change
-    add_column :data_sources, :scale, :float, null: false, default: 1, after: :role
+    add_column :data_sources, :scale, :float, null: false, default: 1, after: :priority
   end
 end
