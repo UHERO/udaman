45ffa552cb648618dd4a3ab22ac93c2788e5dd1f
dji <David.Iannucci@hawaii.edu>
2023-04-23 15:20:29 -1000

    wip

--- app/lib/helper_utilities.rb
+++ app/lib/helper_utilities.rb
@@ -3,21 +3,21 @@ module HelperUtilities
   ## e.g. first_month_of_quarter(3) => 7; first_month_of_quarter("Q1") => 1
   def first_month_of_quarter(q)
     qnum = q.to_s.gsub(/\D/,'').to_i  ## allow for caller to pass things like "Q2"
-    raise "first_month_of_quarter: Invalid quarter number in #{q}" unless [1,2,3,4].include? qnum
+    raise 'first_month_of_quarter: Invalid quarter number in %s' % q unless [1,2,3,4].include?(qnum)
     qnum * 3 - 2
   end
 
   ## Put in any month number, get out quarter number
   ## e.g. quarter_by_month(11) => 4
   def quarter_by_month(mon)
-    raise "quarter_by_month: Invalid month number #{mon}" unless 0 < mon.to_i && mon.to_i < 13
+    raise 'quarter_by_month: Invalid month number %s' % mon unless 0 < mon.to_i && mon.to_i < 13
     (mon.to_i - 1) / 3 + 1
   end
 
   ## Put in quarter spec like YYYYQ1, YYYY-Q1, YYYY/Q1, YYYY-Q01, "YYYY Q1", "YYYY Q01", etc
   ## get out date form, e.g. qspec_to_date("2012Q2") => "2012-04-01"
   def qspec_to_date(qstr)
-    return nil unless qstr =~ /([12]\d\d\d)[-.\/ ]*Q0?([1234])/i
+    return nil unless qstr =~ /([12]\d\d\d)[-.\/ ]?Q0?([1234])/i
     '%s-%02d-01' % [$1, first_month_of_quarter($2)]
   end
 
@@ -25,7 +25,7 @@ module HelperUtilities
   ## e.g. date_to_qspec("2018-03-01") => "2018Q1"; date_to_qspec("2018-10-01", "-") => "2018-Q4"
   def date_to_qspec(date, delim = nil)
     unless date.class == Date
-      date = grok_date(date) rescue raise("date_to_qspec: #{date} not a valid date string")
+      date = grok_date(date) rescue raise('date_to_qspec: %s not a valid date string' % date)
     end
     '%s%sQ%d' % [date.year, delim, quarter_by_month(date.mon)]
   end
