ef939138712605dc28ca37e18c945a0e01f8aa78
dji <David.Iannucci@hawaii.edu>
2023-05-01 17:20:29 -1000

    wip

--- db/migrate/220170413025782_add_fs_default_range.rb
+++ db/migrate/220170413025782_add_fs_default_range.rb
@@ -1,6 +1,6 @@
 class AddFsDefaultRange < ActiveRecord::Migration[5.2]
   def change
     add_column :forecast_snapshots, :disp_from, :string, null: true, after: :published
-    add_column :forecast_snapshots, :disp_to,   :string, null: true, after: :disp_start
+    add_column :forecast_snapshots, :disp_to,   :string, null: true, after: :disp_from
   end
 end
