31ad3f494d377d80f8daee61f668856f7e33f0b5
dji <David.Iannucci@hawaii.edu>
2023-05-02 18:34:59 -1000

    wip

--- db/migrate/220170413025781_add_foresnap_display_range.rb
+++ db/migrate/220170413025781_add_foresnap_display_range.rb
@@ -1,4 +1,4 @@
-class AddFsDefaultRange < ActiveRecord::Migration[5.2]
+class AddForesnapDisplayRange < ActiveRecord::Migration[5.2]
   def change
     add_column :forecast_snapshots, :disp_from, :string, null: true, after: :published
     add_column :forecast_snapshots, :disp_to,   :string, null: true, after: :disp_from
