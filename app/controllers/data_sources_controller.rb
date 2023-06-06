b0236b38333c1c483991fc1039cc19823ca53122
dji <David.Iannucci@hawaii.edu>
2023-06-05 19:14:27 -1000

    wip

--- app/controllers/data_sources_controller.rb
+++ app/controllers/data_sources_controller.rb
@@ -15,7 +15,7 @@ class DataSourcesController < ApplicationController
   def do_clear
     cutoff_date = clear_params[:date].nil_blank  ## will be nil when all points are to be cleared
     if cutoff_date && clear_params[:type].blank?
-      redirect_to :clear
+      redirect_to action: :clear
     end
     delete_method_param = cutoff_date ? { clear_params[:type].to_sym => cutoff_date } : {}
     @data_source.delete_data_points(**delete_method_param)  ## double splat for hash
