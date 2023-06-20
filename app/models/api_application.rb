af326e2b8b37572b754f6de954a4b1cf35d6d628
dji <David.Iannucci@hawaii.edu>
2023-06-19 20:22:14 -1000

    wip

--- app/models/api_application.rb
+++ app/models/api_application.rb
@@ -1,6 +1,3 @@
 class ApiApplication < ApplicationRecord
   include Cleaning
-
-  ## this is just a comment to say what this branch is for: it's for adding and editing API keys (and other metadata?),
-  ## so that this stuff doesn't have to be done in DbVis or something. After implementing, get rid of this comment.
 end
