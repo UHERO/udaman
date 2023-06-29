2df36cafaf302a11659926e9d9ea3ae1972ba080
dji <David.Iannucci@hawaii.edu>
2023-06-28 19:36:36 -1000

    wip

--- db/migrate/220170413025782_move_units_to_loader.rb
+++ db/migrate/220170413025782_move_units_to_loader.rb
@@ -1,4 +1,11 @@
 class MoveUnitsToLoader < ActiveRecord::Migration[5.2]
+#
+##
+#
+##  RENUMBER THIS MIGRATION IF NEEDED, DEPENDING ON RELEASE ORDER
+#
+##
+#
   def change
     add_column :data_sources, :scale, :float, null: false, default: 1, after: :priority
   end
