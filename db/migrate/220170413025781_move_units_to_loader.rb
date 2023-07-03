4c08561968fe59f79541a767f6143c2f0af0da36
dji <David.Iannucci@hawaii.edu>
2023-07-02 19:46:56 -1000

    wip

--- db/migrate/220170413025781_move_units_to_loader.rb
+++ db/migrate/220170413025781_move_units_to_loader.rb
@@ -1,6 +1,6 @@
 class MoveUnitsToLoader < ActiveRecord::Migration[5.2]
   def change
-    #add_column :data_sources, :scale, :float, null: false, default: 1, after: :priority
+    add_column :data_sources, :scale, :float, null: false, default: 1, after: :priority
     remove_column :xseries, :units
   end
 end
