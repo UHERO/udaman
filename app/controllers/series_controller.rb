27c399fdf3e39ac7f22019a89ae4585a9ce6893d
calebthewood <calebw787@gmail.com>
2025-08-11 08:54:04 -1000

    shift from b64 search encoding

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -239,7 +239,8 @@ end
   end
 
   def new_search(search_string = nil)
-    @search_string = search_string || helpers.url_decode(params[:search_string])
+    @search_string = search_string || params[:search_string]&.strip
+    @search_string = helpers.url_decode(@search_string) if @search_string&.start_with?('b64:')
     Rails.logger.info { "SEARCHLOG: user #{current_user.username.ljust(9, ' ')} searched #{@search_string}" }
     all_series = Series.search(@search_string, limit: ENV['SEARCH_DEFAULT_LIMIT'].to_i, user: current_user)
     if all_series.count == 1 && @search_string !~ /[+]1\b/
