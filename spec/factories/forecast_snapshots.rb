16ca230a97fd1e313d11095fa2f235ce5f4459cc
UHERO Deploy <uheroweb@hawaii.edu>
2025-12-18 11:26:21 -1000

    Stop tracking bin, vendor, log, and spec directories

--- spec/factories/forecast_snapshots.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :forecast_snapshot do
-    name { 'MyString' }
-    version { 'MyString' }
-    comments { 'MyText' }
-    published { false }
-  end
-end
--- spec/lib/download/integer_pattern_processor_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe Downloaders::IntegerPatternProcessor do  
-  it "should compute the index for an increment pattern correctly" do
-    ip = Downloaders::IntegerPatternProcessor.new("increment:37:1")
-    ip.compute(2).should == 39
-  end
-end
