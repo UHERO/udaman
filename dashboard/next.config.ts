7d5ea1d60afd044ab14045f3a2e97e69e1f8c467
dangabrielle <124424171+dangabrielle@users.noreply.github.com>
2026-01-14 11:22:10 -1000

    revert turbo

--- dashboard/next.config.ts
+++ dashboard/next.config.ts
@@ -9,6 +9,7 @@ const __dirname = dirname(__filename);
 dotenv.config({ path: resolve(__dirname, "../.env") });
 
 const nextConfig: NextConfig = {
+  turbo: { root: "../" },
   env: {
     DB_HOST: process.env.DATABASE_URL,
   },
