54cd73f2f7a6763ff765fa7c10ec1521e6e69d93
calebthewood <calebw787@gmail.com>
2026-01-30 12:25:44 -1000

    reduce concurrent worker threads

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_SIDEKIQ_URL'] || ENV['REDIS_URL'] }
-
+  config.concurrency = 5
   # accepts :expiration (optional)
   Sidekiq::Status.configure_server_middleware config, expiration: 40.minutes
 
